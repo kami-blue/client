2393aa664bc7a33d95f452b681efdad4bf28ee63
Bella <bella.who.two@gmail.com>
2020-02-06 22:43:55 -0500

    rewrite chattimestamp and time for #329

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/ChatTimestamp.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/ChatTimestamp.java
@@ -5,8 +5,10 @@ import me.zero.alpine.listener.Listener;
 import me.zeroeightsix.kami.command.Command;
 import me.zeroeightsix.kami.event.events.PacketEvent;
 import me.zeroeightsix.kami.module.Module;
-import me.zeroeightsix.kami.module.modules.gui.InfoOverlay;
-import me.zeroeightsix.kami.util.InfoCalculator;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
+import me.zeroeightsix.kami.util.ColourUtils;
+import me.zeroeightsix.kami.util.TimeUtil;
 import net.minecraft.network.play.server.SPacketChat;
 import net.minecraft.util.text.TextFormatting;
 
@@ -16,6 +18,11 @@ import net.minecraft.util.text.TextFormatting;
  */
 @Module.Info(name = "ChatTimestamp", category = Module.Category.MISC)
 public class ChatTimestamp extends Module {
+    private Setting<ColourUtils.ColourCode> firstColour = register(Settings.e("First Colour", ColourUtils.ColourCode.GREY));
+    private Setting<ColourUtils.ColourCode> secondColour = register(Settings.e("Second Colour", ColourUtils.ColourCode.WHITE));
+    private Setting<TimeUtil.TimeType> timeTypeSetting = register(Settings.e("Time Format", TimeUtil.TimeType.HHMM));
+    private Setting<TimeUtil.TimeUnit> timeUnitSetting = register(Settings.e("Time Unit", TimeUtil.TimeUnit.h12));
+
     @EventHandler
     public Listener<PacketEvent.Receive> listener = new Listener<>(event -> {
         if (mc.player == null || this.isDisabled()) return;
@@ -28,12 +35,8 @@ public class ChatTimestamp extends Module {
         }
     });
 
-//    public void onUpdate() {
-//        InfoOverlay info = new InfoOverlay();
-//        String colour = InfoOverlay.ColourCode(secondColour.getValue());
-//    }
     private boolean addTime(String message) {
-        Command.sendRawChatMessage("<" + new InfoOverlay().formatChatTime() + "> " + message);
+        Command.sendRawChatMessage("<" + TimeUtil.getFinalTime(secondColour.getValue(), firstColour.getValue(), timeUnitSetting.getValue(), timeTypeSetting.getValue()) + TextFormatting.RESET + "> " + message);
         return true;
     }
 
