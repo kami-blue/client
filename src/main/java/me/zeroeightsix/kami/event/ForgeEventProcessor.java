1760291848f8bd7e89c7c1fbbb56dbd85e259f66
d1gress <dragoclanleader@gmail.com>
2020-02-17 23:00:59 +0800

    preFIXED HAHAHAHAHAA

--- src/main/java/me/zeroeightsix/kami/event/ForgeEventProcessor.java
+++ src/main/java/me/zeroeightsix/kami/event/ForgeEventProcessor.java
@@ -107,14 +107,11 @@ public class ForgeEventProcessor {
     @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
     public void onKeyInput(InputEvent.KeyInputEvent event) {
         if (!Keyboard.getEventKeyState()) return;
-        if (ModuleManager.isModuleEnabled("PrefixChat")) {
-            if (("" + Keyboard.getEventCharacter()).equalsIgnoreCase(Command.getCommandPrefix())) {
-                if (!Minecraft.getMinecraft().player.isSneaking() && (boolean) ModuleManager.getModuleByName("PrefixChat").getSettingByName("disableOnSneak").getValue()) {
-                    Minecraft.getMinecraft().displayGuiScreen(new GuiChat(Command.getCommandPrefix()));
-                }
-            } else {
-                ModuleManager.onBind(Keyboard.getEventKey());
-            }
+        if (ModuleManager.isModuleEnabled("PrefixChat") && ("" + Keyboard.getEventCharacter()).equalsIgnoreCase(Command.getCommandPrefix()) && !(Minecraft.getMinecraft().player.isSneaking() && (boolean) ModuleManager.getModuleByName("PrefixChat").getSettingByName("disableOnSneak").getValue())) {
+            Minecraft.getMinecraft().displayGuiScreen(new GuiChat(Command.getCommandPrefix()));
+            return;
+        } else {
+            ModuleManager.onBind(Keyboard.getEventKey());
         }
     }
 
