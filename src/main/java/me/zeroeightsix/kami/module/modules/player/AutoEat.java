62a3ece60e75a96167b60190e43b6e60284e0d09
Bella <bella.who.two@gmail.com>
2020-03-09 10:05:33 -0400

    close #545

--- src/main/java/me/zeroeightsix/kami/module/modules/player/AutoEat.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/player/AutoEat.java
@@ -1,6 +1,8 @@
 package me.zeroeightsix.kami.module.modules.player;
 
 import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
@@ -11,15 +13,18 @@ import net.minecraft.util.FoodStats;
 
 /**
  * Created by 086 on 8/04/2018.
+ * Updated by polymer on 09/04/20
+ * Updated by S-B99 on 09/04/20
  */
 @Module.Info(name = "AutoEat", description = "Automatically eat when hungry", category = Module.Category.PLAYER)
 public class AutoEat extends Module {
+    private Setting<Integer> foodLevel = register(Settings.integerBuilder("Eat level").withValue(15).withMinimum(1).withMaximum(20).build());
 	
     private int lastSlot = -1;
     private boolean eating = false;
 
     private boolean isValid(ItemStack stack, int food) {
-        return (stack.getItem() instanceof ItemFood && (20 - food) >= ((ItemFood) stack.getItem()).getHealAmount(stack)) && passItemCheck(stack.getItem());
+        return (stack.getItem() instanceof ItemFood && (foodLevel.getValue()- food) >= ((ItemFood) stack.getItem()).getHealAmount(stack)) && passItemCheck(stack.getItem());
     }
 
     private boolean passItemCheck(Item item) {
