b0291649db17a4e4c904cb84dcb7eaecc77de438
Dewy REDACTED <dljcoolguy@gmail.com>
2020-04-22 13:54:55 +0100

    added baritone config module

--- src/main/java/me/zeroeightsix/kami/module/modules/client/Baritone.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/client/Baritone.java
@@ -1,4 +1,19 @@
 package me.zeroeightsix.kami.module.modules.client;
 
-public class Baritone {
+import me.zeroeightsix.kami.module.Module;
+import me.zeroeightsix.kami.util.MessageSendHelper;
+
+@Module.Info(
+          name = "Baritone",
+          category = Module.Category.CLIENT,
+          description = "Configure KAMI Blue's Baritone integration.",
+          showOnArray = Module.ShowOnArray.OFF
+)
+public class Baritone extends Module {
+
+    @Override
+    protected void onDisable() {
+        MessageSendHelper.sendErrorMessage("Error: The Baritone module is for configuring Baritone integration, not toggling it.");
+        enable();
+    }
 }
