6b6c42f4122c41e1a1d4b6d8718dc8970a9a0e47
suretic <37001138+suretic@users.noreply.github.com>
2020-05-13 21:53:59 +0200

    Update Spammer.java

--- src/main/java/me/zeroeightsix/kami/module/modules/chat/Spammer.java
+++ src/main/java/me/zeroeightsix/kami/module/modules/chat/Spammer.java
@@ -6,7 +6,8 @@ import me.zeroeightsix.kami.setting.Settings;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
