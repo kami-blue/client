d7b23b435d160decafe817978f25bb47031f5b11
sourTaste000 <sourTaste000@gmail.com>
2020-06-25 20:09:08 -0700

    Proven small brain moments™

---
+++ src/main/java/me/zeroeightsix/kami/module/modules/player/TpsSync.kt
@@ -0,0 +1,20 @@
+package me.zeroeightsix.kami.module.modules.player
+
+import me.zeroeightsix.kami.module.Module
+
+/**
+ * Created by 086 on 9/04/2018.
+ */
+@Module.Info(
+        name = "TpsSync",
+        description = "Synchronizes block states with the server TPS",
+        category = Module.Category.PLAYER
+)
+class TpsSync : Module() {
+    companion object {
+        private lateinit var INSTANCE: TpsSync
+        fun isSync(): Boolean { return INSTANCE.isEnabled }
+    }
+
+    fun TpsSync() { INSTANCE = this }
+}
