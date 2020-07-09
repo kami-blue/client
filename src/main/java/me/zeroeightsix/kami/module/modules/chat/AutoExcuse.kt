ad50a1de6df92e574f650d283cf62b5d24e09765
sourTaste000 <47074495+sourTaste000@users.noreply.github.com>
2020-07-09 12:12:47 -0700

    format

--- src/main/java/me/zeroeightsix/kami/module/modules/chat/AutoExcuse.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/chat/AutoExcuse.kt
@@ -16,7 +16,11 @@ import java.util.*
  * @since 7/8/2020
  * most :smoothbrain: code ever
  */
-@Module.Info(name = "AutoExcuse", description = "Makes an excuse for you when you die", category = Module.Category.CHAT)
+@Module.Info(
+        name = "AutoExcuse",
+        description = "Makes an excuse for you when you die",
+        category = Module.Category.CHAT
+)
 class AutoExcuse : Module() {
 
     private val rand = Random()
@@ -26,7 +30,10 @@ class AutoExcuse : Module() {
     @EventHandler
     var listener = Listener(EventHook { event: Displayed ->
         if (event.screen is GuiGameOver) {
+            //can't send message when player died
             mc.player.respawnPlayer()
+
+            //just change the bound to add more
             when (modeSetting.value) {
                 Mode.CRYSTAL -> when (rand.nextInt(3)) {
                     0 -> MessageSendHelper.sendServerMessage("my ping is so bad")
@@ -34,12 +41,14 @@ class AutoExcuse : Module() {
                     2 -> MessageSendHelper.sendServerMessage("why did my autototem break")
                     3 -> MessageSendHelper.sendServerMessage("i was desynced")
                 }
+
                 Mode.ANARCHY -> when (rand.nextInt(3)) {
                     0 -> MessageSendHelper.sendServerMessage("i hate withers")
                     1 -> MessageSendHelper.sendServerMessage("im trying to escape why did u kill me :((")
                     2 -> MessageSendHelper.sendServerMessage("can someone give me food")
                     3 -> MessageSendHelper.sendServerMessage("ouch i broke my legs")
                 }
+
             }
         }
     })
