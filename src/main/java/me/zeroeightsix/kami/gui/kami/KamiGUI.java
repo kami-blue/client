cedd85f040bfeb4c5379929178a3b2fb254bd3cc
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-07-19 18:53:53 -0400

    Make text in baritone process gui shorter

--- src/main/java/me/zeroeightsix/kami/gui/kami/KamiGUI.java
+++ src/main/java/me/zeroeightsix/kami/gui/kami/KamiGUI.java
@@ -303,9 +303,9 @@ public class KamiGUI extends GUI {
 
             if (!frameFinal.isMinimized() && BaritoneAPI.getProvider().getPrimaryBaritone().getPathingControlManager().mostRecentInControl().isPresent()) {
                 if (MODULE_MANAGER.isModuleEnabled(AutoWalk.class) && MODULE_MANAGER.getModuleT(AutoWalk.class).mode.getValue().equals(AutoWalk.AutoWalkMode.BARITONE)) {
-                    processes.addLine("Current Process: AutoWalk (" + AutoWalk.direction + ")");
+                    processes.addLine("Process: AutoWalk (" + AutoWalk.direction + ")");
                 } else {
-                    processes.addLine("Current Process: " + BaritoneAPI.getProvider().getPrimaryBaritone().getPathingControlManager().mostRecentInControl().get().displayName());
+                    processes.addLine("Process: " + BaritoneAPI.getProvider().getPrimaryBaritone().getPathingControlManager().mostRecentInControl().get().displayName());
                 }
             }
         });
