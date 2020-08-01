9508e34f85eb1676a23e153fd452889d8cb5dbc9
Dewy <dewy@dewy.dev>
2020-08-01 17:21:36 +0100

    add emoji module

---
+++ src/main/java/me/zeroeightsix/kami/module/modules/chat/Emoji.java
@@ -0,0 +1,10 @@
+package me.zeroeightsix.kami.module.modules.chat;
+
+import me.zeroeightsix.kami.module.Module;
+
+@Module.Info(
+        name = "Emoji",
+        description = "Add emojis to chat, courtesy of the EmojiAPI.",
+        category = Module.Category.CHAT
+)
+public class Emoji extends Module {}
