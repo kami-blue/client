4814821aa9725e16f32e1f0059e4082cab13d0b5
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-18 16:30:04 -0400

    Kotlinized MessageDetectionHelper

--- src/main/java/me/zeroeightsix/kami/util/MessageDetectionHelper.kt
+++ src/main/java/me/zeroeightsix/kami/util/MessageDetectionHelper.kt
@@ -1,6 +1,6 @@
-package me.zeroeightsix.kami.util;
+package me.zeroeightsix.kami.util
 
-import java.util.regex.Pattern;
+import java.util.regex.Pattern
 
 /**
  * A helper to detect certain messages and return a boolean or message
@@ -8,47 +8,48 @@ import java.util.regex.Pattern;
  * @author dominikaaaa
  * @see me.zeroeightsix.kami.module.modules.chat.DiscordNotifs
  */
-public class MessageDetectionHelper {
-    public static String getMessageType(boolean direct, boolean directSent, String message, String server) {
-        if (isDirect(direct, message)) return "You got a direct message!\n";
-        if (isDirectOther(directSent, message)) return "You sent a direct message!\n";
-        if (message.equals("KamiBlueMessageType1")) return "Connected to " + server;
-        if (message.equals("KamiBlueMessageType2")) return "Disconnected from " + server;
-        return "";
+object MessageDetectionHelper {
+    @JvmStatic
+    fun getMessageType(direct: Boolean, directSent: Boolean, message: String, server: String): String {
+        if (isDirect(direct, message)) return "You got a direct message!\n"
+        if (isDirectOther(directSent, message)) return "You sent a direct message!\n"
+        if (message == "KamiBlueMessageType1") return "Connected to $server"
+        return if (message == "KamiBlueMessageType2") "Disconnected from $server" else ""
     }
 
-    public static boolean isDirect(boolean direct, String message) {
-        return direct && Pattern.compile("^([0-9A-z_])+ whispers:.*").matcher(message).find();
+    @JvmStatic
+    fun isDirect(direct: Boolean, message: String?): Boolean {
+        if (message == null) return false
+        return direct && Pattern.compile("^([0-9A-z_])+ whispers:.*").matcher(message).find()
     }
 
-    public static boolean isDirectOther(boolean directSent, String message) {
-        return directSent && Pattern.compile("^to ([0-9A-z_])+:.*").matcher(message).find();
+    @JvmStatic
+    fun isDirectOther(directSent: Boolean, message: String?): Boolean {
+        if (message == null) return false
+        return directSent && Pattern.compile("^to ([0-9A-z_])+:.*").matcher(message).find()
     }
 
-    public static boolean isTPA(boolean tpa, String message) {
-        return tpa && Pattern.compile("^([0-9A-z_])+ (has requested|wants) to teleport to you\\..*").matcher(message).find();
+    fun isTPA(tpa: Boolean, message: String?): Boolean {
+        if (message == null) return false
+        return tpa && Pattern.compile("^([0-9A-z_])+ (has requested|wants) to teleport to you\\..*").matcher(message).find()
     }
 
-    public static boolean isQueue(boolean queue, String message) {
-        if (queue && message.contains("Position in queue:")) return true;
-        else return queue && message.contains("2b2t is full");
+    fun isQueue(queue: Boolean, message: String): Boolean {
+        return if (queue && message.contains("Position in queue:")) true else queue && message.contains("2b2t is full")
     }
 
-    public static boolean isImportantQueue(boolean importantPings, String message) {
-        return importantPings && (
-                message.equals("Position in queue: 1") ||
-                        message.equals("Position in queue: 2") ||
-                        message.equals("Position in queue: 3"));
+    @JvmStatic
+    fun isImportantQueue(importantPings: Boolean, message: String): Boolean {
+        return importantPings && (message == "Position in queue: 1" || message == "Position in queue: 2" || message == "Position in queue: 3")
     }
 
-    public static boolean isRestart(boolean restart, String message) {
-        return restart && message.contains("[SERVER] Server restarting in");
+    @JvmStatic
+    fun isRestart(restart: Boolean, message: String): Boolean {
+        return restart && message.contains("[SERVER] Server restarting in")
     }
 
-    public static boolean shouldSend(boolean all, boolean restart, boolean direct, boolean directSent, boolean queue, boolean importantPings, String message) {
-        if (all) return true;
-        else
-            return isRestart(restart, message) || isDirect(direct, message) || isDirectOther(directSent, message) || isQueue(queue, message) || isImportantQueue(importantPings, message);
+    @JvmStatic
+    fun shouldSend(all: Boolean, restart: Boolean, direct: Boolean, directSent: Boolean, queue: Boolean, importantPings: Boolean, message: String): Boolean {
+        return if (all) true else isRestart(restart, message) || isDirect(direct, message) || isDirectOther(directSent, message) || isQueue(queue, message) || isImportantQueue(importantPings, message)
     }
-
 }
