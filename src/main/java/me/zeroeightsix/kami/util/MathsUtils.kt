949596651eac657274e31cd292e2a896d0801434
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-18 16:21:20 -0400

    Kotlinized MathsUtils

--- src/main/java/me/zeroeightsix/kami/util/MathsUtils.kt
+++ src/main/java/me/zeroeightsix/kami/util/MathsUtils.kt
@@ -1,141 +1,114 @@
-package me.zeroeightsix.kami.util;
+package me.zeroeightsix.kami.util
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.client.Minecraft
+import net.minecraft.util.math.BlockPos
+import kotlin.math.*
 
 /**
  * Created by Dewy on the 17th of April, 2020
- * Updated by Xiaro on 21/07/20
+ * Updated by Xiaro on 18/08/20
  */
-public class MathsUtils {
-
-    public static BlockPos mcPlayerPosFloored(Minecraft mc) {
-        return new BlockPos(Math.floor(mc.player.posX), Math.floor(mc.player.posY), Math.floor(mc.player.posZ));
+object MathsUtils {
+    fun mcPlayerPosFloored(mc: Minecraft): BlockPos {
+        return BlockPos(floor(mc.player.posX), floor(mc.player.posY), floor(mc.player.posZ))
     }
 
-    public static double normalizeAngle(double angleIn) {
-        angleIn = angleIn % 360.0D;
-
-        if (angleIn >= 180.0D) {
-            angleIn -= 360.0D;
+    fun normalizeAngle(angleIn: Double): Double {
+        var angle = angleIn
+        angle %= 360.0
+        if (angle >= 180.0) {
+            angle -= 360.0
         }
-
-        if (angleIn < -180.0D) {
-            angleIn += 360.0D;
+        if (angle < -180.0) {
+            angle += 360.0
         }
-
-        return angleIn;
+        return angle
     }
 
-    public static double round(float value, int places) {
-        double scale = Math.pow(10, places);
-        return Math.round(value * scale) / scale;
+    fun round(value: Float, places: Int): Double {
+        val scale = 10.0.pow(places.toDouble())
+        return (value * scale).roundToInt() / scale
     }
 
-    public static double round(double value, int places) {
-        double scale = Math.pow(10, places);
-        return Math.round(value * scale) / scale;
+    @JvmStatic
+    fun round(value: Double, places: Int): Double {
+        val scale = 10.0.pow(places.toDouble())
+        return round(value * scale) / scale
     }
 
-    public static boolean isNumberEven(int i) {
-        return (i & 1) == 0;
+    @JvmStatic
+    fun isNumberEven(i: Int): Boolean {
+        return i and 1 == 0
     }
 
-    public static int reverseNumber(int num, int min, int max) {
-        return (max + min) - num;
+    @JvmStatic
+    fun reverseNumber(num: Int, min: Int, max: Int): Int {
+        return max + min - num
     }
 
-    public static boolean isBetween(int min, int max, int value) {
-        return value >= min && value <= max;
+    fun isBetween(min: Int, max: Int, value: Int): Boolean {
+        return value in min..max
     }
 
-    public static boolean isBetween(double min, double max, double value) {
-        return value >= min && value <= max;
+    fun isBetween(min: Double, max: Double, value: Double): Boolean {
+        return value in min..max
     }
 
-    public static Cardinal getPlayerCardinal(Minecraft mc) {
-        if (isBetween(-22.5, 22.5, normalizeAngle(mc.player.rotationYaw))) {
-            return Cardinal.POS_Z;
-        } else if (isBetween(22.6, 67.5, normalizeAngle(mc.player.rotationYaw))) {
-            return Cardinal.NEG_X_POS_Z;
-        } else if (isBetween(67.6, 112.5, normalizeAngle(mc.player.rotationYaw))) {
-            return Cardinal.NEG_X;
-        } else if (isBetween(112.6, 157.5, normalizeAngle(mc.player.rotationYaw))) {
-            return Cardinal.NEG_X_NEG_Z;
-        } else if (normalizeAngle(mc.player.rotationYaw) >= 157.6 || normalizeAngle(mc.player.rotationYaw) <= -157.5) {
-            return Cardinal.NEG_Z;
-        } else if (isBetween(-157.6, -112.5, normalizeAngle(mc.player.rotationYaw))) {
-            return Cardinal.POS_X_NEG_Z;
-        } else if (isBetween(-112.5, -67.5, normalizeAngle(mc.player.rotationYaw))) {
-            return Cardinal.POS_X;
-        } else if (isBetween(-67.6, -22.6, normalizeAngle(mc.player.rotationYaw))) {
-            return Cardinal.POS_X_POS_Z;
+    @JvmStatic
+    fun getPlayerCardinal(mc: Minecraft): Cardinal {
+        return if (isBetween(-22.5, 22.5, normalizeAngle(mc.player.rotationYaw.toDouble()))) {
+            Cardinal.POS_Z
+        } else if (isBetween(22.6, 67.5, normalizeAngle(mc.player.rotationYaw.toDouble()))) {
+            Cardinal.NEG_X_POS_Z
+        } else if (isBetween(67.6, 112.5, normalizeAngle(mc.player.rotationYaw.toDouble()))) {
+            Cardinal.NEG_X
+        } else if (isBetween(112.6, 157.5, normalizeAngle(mc.player.rotationYaw.toDouble()))) {
+            Cardinal.NEG_X_NEG_Z
+        } else if (normalizeAngle(mc.player.rotationYaw.toDouble()) >= 157.6 || normalizeAngle(mc.player.rotationYaw.toDouble()) <= -157.5) {
+            Cardinal.NEG_Z
+        } else if (isBetween(-157.6, -112.5, normalizeAngle(mc.player.rotationYaw.toDouble()))) {
+            Cardinal.POS_X_NEG_Z
+        } else if (isBetween(-112.5, -67.5, normalizeAngle(mc.player.rotationYaw.toDouble()))) {
+            Cardinal.POS_X
+        } else if (isBetween(-67.6, -22.6, normalizeAngle(mc.player.rotationYaw.toDouble()))) {
+            Cardinal.POS_X_POS_Z
         } else {
-            return Cardinal.ERROR;
+            Cardinal.ERROR
         }
     }
 
-    public static CardinalMain getPlayerMainCardinal(Minecraft mc) {
-        char cardinal = Character.toUpperCase(mc.player.getHorizontalFacing().toString().charAt(0));
-        switch (cardinal) {
-            case 'N':
-                return MathsUtils.CardinalMain.NEG_Z;
-            case 'S':
-                return MathsUtils.CardinalMain.POS_Z;
-            case 'E':
-                return MathsUtils.CardinalMain.POS_X;
-            case 'W':
-                return MathsUtils.CardinalMain.NEG_X;
-            default:
-                return null;
+    fun getPlayerMainCardinal(mc: Minecraft): CardinalMain? {
+        return when (Character.toUpperCase(mc.player.horizontalFacing.toString()[0])) {
+            'N' -> CardinalMain.NEG_Z
+            'S' -> CardinalMain.POS_Z
+            'E' -> CardinalMain.POS_X
+            'W' -> CardinalMain.NEG_X
+            else -> null
         }
     }
 
-    public enum Cardinal {
-        POS_Z("+Z"),
-        NEG_X_POS_Z("-X / +Z"),
-        NEG_X("-X"),
-        NEG_X_NEG_Z("-X / -Z"),
-        NEG_Z("-Z"),
-        POS_X_NEG_Z("+X / -Z"),
-        POS_X("+X"),
-        POS_X_POS_Z("+X / +Z"),
-        ERROR("ERROR_CALC_DIRECT");
-
-        public String cardinalName;
-
-        Cardinal(String cardinalName) {
-            this.cardinalName = cardinalName;
-        }
+    fun convertRange(valueIn: Int, minIn: Int, maxIn: Int, minOut: Int, maxOut: Int): Int {
+        return convertRange(valueIn.toDouble(), minIn.toDouble(), maxIn.toDouble(), minOut.toDouble(), maxOut.toDouble()).toInt()
     }
 
-    public enum CardinalMain {
-        POS_Z("+Z"),
-        NEG_X("-X"),
-        NEG_Z("-Z"),
-        POS_X("+X");
-
-        public String cardinalName;
-
-        CardinalMain(String cardinalName) {
-            this.cardinalName = cardinalName;
-        }
+    fun convertRange(valueIn: Float, minIn: Float, maxIn: Float, minOut: Float, maxOut: Float): Float {
+        return convertRange(valueIn.toDouble(), minIn.toDouble(), maxIn.toDouble(), minOut.toDouble(), maxOut.toDouble()).toFloat()
     }
 
-    public static int convertRange(int valueIn, int minIn, int maxIn, int minOut, int maxOut) {
-        return (int) convertRange((double) valueIn, minIn, maxIn, minOut, maxOut);
+    fun convertRange(valueIn: Double, minIn: Double, maxIn: Double, minOut: Double, maxOut: Double): Double {
+        val rangeIn = maxIn - minIn
+        val rangeOut = maxOut - minOut
+        val convertedIn = (valueIn - minIn) * (rangeOut / rangeIn) + minOut
+        val actualMin = min(minOut, maxOut)
+        val actualMax = max(minOut, maxOut)
+        return min(max(convertedIn, actualMin), actualMax)
     }
 
-    public static float convertRange(float valueIn, float minIn, float maxIn, float minOut, float maxOut) {
-        return (float) convertRange((double) valueIn, minIn, maxIn, minOut, maxOut);
+    enum class Cardinal(var cardinalName: String) {
+        POS_Z("+Z"), NEG_X_POS_Z("-X / +Z"), NEG_X("-X"), NEG_X_NEG_Z("-X / -Z"), NEG_Z("-Z"), POS_X_NEG_Z("+X / -Z"), POS_X("+X"), POS_X_POS_Z("+X / +Z"), ERROR("ERROR_CALC_DIRECT");
     }
 
-    public static double convertRange(double valueIn, double minIn, double maxIn, double minOut, double maxOut) {
-        final double rangeIn = maxIn - minIn;
-        final double rangeOut = maxOut - minOut;
-        final double convertedIn = ((valueIn - minIn) * (rangeOut / rangeIn) + minOut);
-        final double acutalMin = Math.min(minOut, maxOut);
-        final double acutalMax = Math.max(minOut, maxOut);
-        return Math.min(Math.max(convertedIn, acutalMin), acutalMax);
+    enum class CardinalMain(var cardinalName: String) {
+        POS_Z("+Z"), NEG_X("-X"), NEG_Z("-Z"), POS_X("+X");
     }
 }
