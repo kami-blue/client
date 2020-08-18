8b1b950519f1ca196926a2e832bf0aef0da4d5af
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-18 19:10:20 -0400

    Make ClickGUI shown on client category for easier rebinding

--- src/main/java/me/zeroeightsix/kami/module/modules/ClickGUI.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/ClickGUI.kt
@@ -2,25 +2,36 @@ package me.zeroeightsix.kami.module.modules
 
 import me.zeroeightsix.kami.gui.kami.DisplayGuiScreen
 import me.zeroeightsix.kami.module.Module
+import me.zeroeightsix.kami.module.ModuleManager.getModuleT
+import me.zeroeightsix.kami.setting.Setting
 import me.zeroeightsix.kami.setting.Settings
+import me.zeroeightsix.kami.util.MathsUtils
+import me.zeroeightsix.kami.util.Wrapper.minecraft
+import net.minecraft.client.gui.ScaledResolution
 import org.lwjgl.input.Keyboard
 
 /**
  * Created by 086 on 23/08/2017.
+ * Updated by Xiaro on 18/08/20
  */
 @Module.Info(
-        name = "clickGUI",
+        name = "ClickGUI",
         description = "Opens the Click GUI",
-        category = Module.Category.CLIENT
+        category = Module.Category.CLIENT,
+        alwaysListening = true
 )
 class ClickGUI : Module() {
-    private val scale = register(Settings.floatBuilder("Scale").withValue(1f).withRange(0f, 4f).build())
 
     override fun onEnable() {
         if (mc.currentScreen !is DisplayGuiScreen) {
             mc.displayGuiScreen(DisplayGuiScreen(mc.currentScreen))
         }
-        disable()
+    }
+
+    override fun onDisable() {
+        if (mc.currentScreen is DisplayGuiScreen) {
+            (mc.currentScreen as DisplayGuiScreen).closeGui()
+        }
     }
 
     init {
