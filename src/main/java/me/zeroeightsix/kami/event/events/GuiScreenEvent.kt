b6e262131a0a77a3e0c8b4a399e2747d68281d4a
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-18 21:58:40 -0400

    GuiScreen should be nullable

--- src/main/java/me/zeroeightsix/kami/event/events/GuiScreenEvent.kt
+++ src/main/java/me/zeroeightsix/kami/event/events/GuiScreenEvent.kt
@@ -6,8 +6,8 @@ import net.minecraft.client.gui.GuiScreen
  * Created by 086 on 17/11/2017.
  * Updated by Xiaro on 18/08/20
  */
-open class GuiScreenEvent(var screen: GuiScreen) {
+open class GuiScreenEvent(var screen: GuiScreen?) {
 
-    class Displayed(screen: GuiScreen) : GuiScreenEvent(screen)
-    class Closed(screen: GuiScreen) : GuiScreenEvent(screen)
+    class Displayed(screen: GuiScreen?) : GuiScreenEvent(screen)
+    class Closed(screen: GuiScreen?) : GuiScreenEvent(screen)
 }
