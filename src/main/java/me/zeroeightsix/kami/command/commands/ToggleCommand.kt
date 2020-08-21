a14fa91a097d87b3a47fda016fe90ecc16a2aae6
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-21 02:14:26 -0400

    Converted ToggleCommand to Kotlin Fixed ;toggle module not found

--- src/main/java/me/zeroeightsix/kami/command/commands/ToggleCommand.kt
+++ src/main/java/me/zeroeightsix/kami/command/commands/ToggleCommand.kt
@@ -1,44 +1,40 @@
-package me.zeroeightsix.kami.command.commands;
+package me.zeroeightsix.kami.command.commands
 
-import me.zeroeightsix.kami.command.Command;
-import me.zeroeightsix.kami.command.syntax.ChunkBuilder;
-import me.zeroeightsix.kami.command.syntax.parsers.ModuleParser;
-import me.zeroeightsix.kami.module.Module;
-import me.zeroeightsix.kami.module.ModuleManager;
-import me.zeroeightsix.kami.module.modules.client.CommandConfig;
-
-import static me.zeroeightsix.kami.KamiMod.MODULE_MANAGER;
-import static me.zeroeightsix.kami.util.text.MessageSendHelper.sendChatMessage;
+import me.zeroeightsix.kami.command.Command
+import me.zeroeightsix.kami.command.syntax.ChunkBuilder
+import me.zeroeightsix.kami.command.syntax.parsers.ModuleParser
+import me.zeroeightsix.kami.module.Module
+import me.zeroeightsix.kami.module.ModuleManager
+import me.zeroeightsix.kami.module.ModuleManager.ModuleNotFoundException
+import me.zeroeightsix.kami.module.ModuleManager.getModule
+import me.zeroeightsix.kami.module.modules.client.CommandConfig
+import me.zeroeightsix.kami.util.text.MessageSendHelper.sendChatMessage
 
 /**
  * Created by 086 on 17/11/2017.
  */
-public class ToggleCommand extends Command {
+class ToggleCommand : Command("toggle", ChunkBuilder()
+        .append("module", true, ModuleParser())
+        .build(), "t") {
 
-    public ToggleCommand() {
-        super("toggle", new ChunkBuilder()
-                .append("module", true, new ModuleParser())
-                .build(), "t");
-        setDescription("Quickly toggle a module on and off");
+    override fun call(args: Array<String>) {
+        if (args.isEmpty()) {
+            sendChatMessage("Please specify a module!")
+            return
+        }
+
+        try {
+            val module = getModule(args[0])?: return
+            module.toggle()
+            if (ModuleManager.getModuleT(CommandConfig::class.java)!!.toggleMessages.value) {
+                sendChatMessage(module.name.value + if (module.isEnabled) " &aenabled" else " &cdisabled")
+            }
+        } catch (x: ModuleNotFoundException) {
+            sendChatMessage("Unknown module '" + args[0] + "'")
+        }
     }
 
-    @Override
-    public void call(String[] args) {
-        if (args.length == 0) {
-            sendChatMessage("Please specify a module!");
-            return;
-        }
-        try {
-            Class<?> aClass = Class.forName(args[0]);
-            Module m = MODULE_MANAGER.getModule((Class<? extends Module>) aClass);
-            m.toggle();
-            if (!MODULE_MANAGER.getModuleT(CommandConfig.class).toggleMessages.getValue()) {
-                sendChatMessage(m.name.getValue() + (m.isEnabled() ? " &aenabled" : " &cdisabled"));
-            }
-        } catch (ModuleManager.ModuleNotFoundException x) {
-            sendChatMessage("Unknown module '" + args[0] + "'");
-        } catch (Throwable t) {
-            sendChatMessage("Bruh");
-        }
+    init {
+        setDescription("Quickly toggle a module on and off")
     }
 }
