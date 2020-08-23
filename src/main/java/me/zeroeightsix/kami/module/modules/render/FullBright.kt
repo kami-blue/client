e340fe4475a928c4cb72a23df44f007da1f20ea1
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-22 22:56:54 -0400

    Rewrote FullBright in Kotlin Added option to change FullBright brightness

--- src/main/java/me/zeroeightsix/kami/module/modules/render/FullBright.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/render/FullBright.kt
@@ -1,120 +1,66 @@
-package me.zeroeightsix.kami.module.modules.render;
+package me.zeroeightsix.kami.module.modules.render
 
-import me.zeroeightsix.kami.module.Module;
-import me.zeroeightsix.kami.setting.Setting;
-import me.zeroeightsix.kami.setting.Settings;
-
-import java.util.Stack;
-import java.util.function.Function;
+import me.zeroeightsix.kami.module.Module
+import me.zeroeightsix.kami.setting.Settings
+import me.zeroeightsix.kami.util.MathsUtils
+import me.zeroeightsix.kami.util.MessageSendHelper.sendChatMessage
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.round
 
 /**
- * Created by 086 on 12/12/2017.
- *
- * @see me.zeroeightsix.kami.mixin.client.MixinEntityRenderer
+ * Created by 086 on 12/12/2017
+ * Rewritten by Xiaro on 22/08/20
  */
 @Module.Info(
         name = "FullBright",
         description = "Makes everything brighter!",
-        category = Module.Category.RENDER
+        category = Module.Category.RENDER,
+        alwaysListening = true
 )
-public class Brightness extends Module {
+class FullBright : Module() {
+    private val gamma = register(Settings.floatBuilder("Gamma").withValue(12f).withRange(1f, 15f).build())
+    private val transitionLength = register(Settings.floatBuilder("TransitionLength").withValue(3f).withRange(0f, 10f).build())
+    private val oldValue = register(Settings.floatBuilder("OldValue").withValue(1f).withRange(0f, 1f).withVisibility { false }.build())
 
-    private Setting<Boolean> transition = register(Settings.b("Transition", true));
-    private Setting<Float> seconds = register(Settings.floatBuilder("Seconds").withMinimum(0f).withMaximum(10f).withValue(5f).withVisibility(o -> transition.getValue()).build());
-    private Setting<Transition> mode = register(Settings.enumBuilder(Transition.class).withName("Mode").withValue(Transition.SINE).withVisibility(o -> transition.getValue()).build());
-
-    private Stack<Float> transitionStack = new Stack<>();
-
-    private static float currentBrightness = 0;
-    private static boolean inTransition = false;
-
-    private void addTransition(boolean isUpwards) {
-        if (transition.getValue()) {
-            int length = (int) (seconds.getValue() * 20);
-            float[] values;
-            switch (mode.getValue()) {
-                case LINEAR:
-                    values = linear(length, isUpwards);
-                    break;
-                case SINE:
-                    values = sine(length, isUpwards);
-                    break;
-                default:
-                    values = new float[]{0};
-                    break;
-            }
-            for (float v : values) {
-                transitionStack.add(v);
-            }
-
-            inTransition = true;
-        } else {
-            currentBrightness = isEnabled() ? 1 : 0;
+    private var gammaSetting: Float
+        get() = mc.gameSettings.gammaSetting
+        set(gammaIn) {
+            mc.gameSettings.gammaSetting = gammaIn
         }
+
+    override fun onEnable() {
+        oldValue.value = mc.gameSettings.gammaSetting
     }
 
-    @Override
-    protected void onEnable() {
-        super.onEnable();
-        addTransition(true);
-    }
+    override fun onUpdate() {
+        sendChatMessage(gammaSetting.toString())
+        when {
+            isEnabled -> {
+                transition(gamma.value)
+                alwaysListening = true
+            }
 
-    @Override
-    protected void onDisable() {
-        setAlwaysListening(true);
-        super.onDisable();
-        addTransition(false);
-    }
-
-    @Override
-    public void onUpdate() {
-        if (inTransition) {
-            if (transitionStack.isEmpty()) {
-                inTransition = false;
-                setAlwaysListening(false);
-                currentBrightness = isEnabled() ? 1 : 0;
-            } else {
-                currentBrightness = transitionStack.pop();
+            isDisabled && gammaSetting != oldValue.value -> {
+                transition(oldValue.value)
             }
         }
     }
 
-    private float[] createTransition(int length, boolean upwards, Function<Float, Float> function) {
-        float[] transition = new float[length];
-        for (int i = 0; i < length; i++) {
-            float v = function.apply(((float) i / (float) length));
-            if (upwards) v = 1 - v;
-            transition[i] = v;
+    private fun transition(target: Float) {
+        gammaSetting = when {
+            gammaSetting !in 0f..15f -> target
+
+            gammaSetting == target -> return
+
+            gammaSetting < target -> min(gammaSetting + getTransitionAmount(), target)
+
+            else -> max(gammaSetting - getTransitionAmount(), target)
         }
-        return transition;
     }
 
-    private float[] linear(int length, boolean polarity) { // length of 20 = 1 second
-        return createTransition(length, polarity, d -> d);
-    }
-
-    private float sine(float x) { // (sin(pi*x-(pi/2)) + 1) / 2
-        return ((float) Math.sin(Math.PI * x - Math.PI / 2) + 1) / 2;
-    }
-
-    private float[] sine(int length, boolean polarity) {
-        return createTransition(length, polarity, this::sine);
-    }
-
-    public static float getCurrentBrightness() {
-        return currentBrightness;
-    }
-
-    public static boolean isInTransition() {
-        return inTransition;
-    }
-
-    public static boolean shouldBeActive() {
-        return isInTransition() || currentBrightness == 1; // if in transition or enabled
-    }
-
-    public enum Transition {
-        LINEAR, SINE;
-
+    private fun getTransitionAmount(): Float {
+        if (transitionLength.value == 0f) return 15f
+        return (1f / transitionLength.value / 20f) * (gamma.value - oldValue.value)
     }
 }
