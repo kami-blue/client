35f8704c28a3d87b48a87b34fcf9a4cd679b3380
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-25 14:58:00 -0400

    Added VertexHelper for support with both VBOs and legacy drawing mode

---
+++ src/main/java/me/zeroeightsix/kami/util/graphics/VertexHelper.kt
@@ -0,0 +1,47 @@
+package me.zeroeightsix.kami.util.graphics
+
+import me.zeroeightsix.kami.util.colourUtils.ColourHolder
+import me.zeroeightsix.kami.util.graphics.RenderUtils.beginBuffer
+import me.zeroeightsix.kami.util.graphics.RenderUtils.endBuffer
+import me.zeroeightsix.kami.util.math.Vec2d
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats
+import net.minecraft.util.math.Vec3d
+import org.lwjgl.opengl.GL11.*
+
+class VertexHelper(private val useVbo: Boolean) {
+
+    private val buffer = RenderUtils.buffer
+
+    fun begin(mode: Int) {
+        if (useVbo) {
+            beginBuffer(mode, DefaultVertexFormats.POSITION_COLOR)
+        } else {
+            glBegin(mode)
+        }
+    }
+
+    fun put(pos: Vec3d, color: ColourHolder) {
+        put(pos.x, pos.y, pos.z, color)
+    }
+
+    fun put(pos: Vec2d, color: ColourHolder) {
+        put(pos.x, pos.y, 0.0, color)
+    }
+
+    fun put(x: Double, y: Double, z: Double, color: ColourHolder) {
+        if (useVbo) {
+            buffer.pos(x, y, z).color(color.r, color.g, color.b, color.a).endVertex()
+        } else {
+            color.setGLColour()
+            glVertex3d(x, y, z)
+        }
+    }
+
+    fun end() {
+        if (useVbo) {
+            endBuffer()
+        } else {
+            glEnd()
+        }
+    }
+}
