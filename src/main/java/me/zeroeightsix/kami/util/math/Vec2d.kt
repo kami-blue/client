506ff001a8e3073a39cd6f36a5a7af47fdfe9422
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-26 01:52:50 -0400

    Rewrote Vec2d to make it actually works

--- src/main/java/me/zeroeightsix/kami/util/math/Vec2d.kt
+++ src/main/java/me/zeroeightsix/kami/util/math/Vec2d.kt
@@ -1,14 +1,16 @@
 package me.zeroeightsix.kami.util.math
 
-import com.sun.javafx.geom.Vec2d
 import net.minecraft.util.math.Vec3d
 import kotlin.math.pow
 import kotlin.math.sqrt
 
-class Vec2d : Vec2d {
+class Vec2d {
+    var x: Double = 0.0
+    var y: Double = 0.0
 
     constructor(x: Double, y: Double) {
-        Vec2d(x, y)
+        this.x = x
+        this.y = y
     }
 
     constructor(vec3d: Vec3d) {
@@ -27,67 +29,71 @@ class Vec2d : Vec2d {
         return (this.x.pow(2) + this.y.pow(2))
     }
 
-    fun divide(vec3d: Vec3d): me.zeroeightsix.kami.util.math.Vec2d {
+    fun divide(vec3d: Vec3d): Vec2d {
         return add(vec3d.x, vec3d.y)
     }
 
-    fun divide(vec2d: Vec2d): me.zeroeightsix.kami.util.math.Vec2d {
+    fun divide(vec2d: Vec2d): Vec2d {
         return add(vec2d.x, vec2d.y)
     }
 
-    fun divide(add: Double): me.zeroeightsix.kami.util.math.Vec2d {
+    fun divide(add: Double): Vec2d {
         return add(add, add)
     }
 
-    fun divide(x: Double, y: Double): me.zeroeightsix.kami.util.math.Vec2d {
-        return me.zeroeightsix.kami.util.math.Vec2d(this.x / x, this.y / y)
+    fun divide(x: Double, y: Double): Vec2d {
+        return Vec2d(this.x / x, this.y / y)
     }
 
-    fun multiply(vec3d: Vec3d): me.zeroeightsix.kami.util.math.Vec2d {
+    fun multiply(vec3d: Vec3d): Vec2d {
         return add(vec3d.x, vec3d.y)
     }
 
-    fun multiply(vec2d: Vec2d): me.zeroeightsix.kami.util.math.Vec2d {
+    fun multiply(vec2d: Vec2d): Vec2d {
         return add(vec2d.x, vec2d.y)
     }
 
-    fun multiply(add: Double): me.zeroeightsix.kami.util.math.Vec2d {
+    fun multiply(add: Double): Vec2d {
         return add(add, add)
     }
 
-    fun multiply(x: Double, y: Double): me.zeroeightsix.kami.util.math.Vec2d {
-        return me.zeroeightsix.kami.util.math.Vec2d(this.x * x, this.y * y)
+    fun multiply(x: Double, y: Double): Vec2d {
+        return Vec2d(this.x * x, this.y * y)
     }
 
-    fun subtract(vec3d: Vec3d): me.zeroeightsix.kami.util.math.Vec2d {
+    fun subtract(vec3d: Vec3d): Vec2d {
         return subtract(vec3d.x, vec3d.y)
     }
 
-    fun subtract(vec2d: Vec2d): me.zeroeightsix.kami.util.math.Vec2d {
+    fun subtract(vec2d: Vec2d): Vec2d {
         return subtract(vec2d.x, vec2d.y)
     }
 
-    fun subtract(sub: Double): me.zeroeightsix.kami.util.math.Vec2d {
+    fun subtract(sub: Double): Vec2d {
         return subtract(sub, sub)
     }
 
-    fun subtract(x: Double, y: Double): me.zeroeightsix.kami.util.math.Vec2d {
+    fun subtract(x: Double, y: Double): Vec2d {
         return add(-x, -y)
     }
 
-    fun add(vec3d: Vec3d): me.zeroeightsix.kami.util.math.Vec2d {
+    fun add(vec3d: Vec3d): Vec2d {
         return add(vec3d.x, vec3d.y)
     }
 
-    fun add(vec2d: Vec2d): me.zeroeightsix.kami.util.math.Vec2d {
+    fun add(vec2d: Vec2d): Vec2d {
         return add(vec2d.x, vec2d.y)
     }
 
-    fun add(add: Double): me.zeroeightsix.kami.util.math.Vec2d {
+    fun add(add: Double): Vec2d {
         return add(add, add)
     }
 
-    fun add(x: Double, y: Double): me.zeroeightsix.kami.util.math.Vec2d {
-        return me.zeroeightsix.kami.util.math.Vec2d(this.x + x, this.y + y)
+    fun add(x: Double, y: Double): Vec2d {
+        return Vec2d(this.x + x, this.y + y)
+    }
+
+    override fun toString(): String {
+        return "Vec2d[${this.x}, ${this.y}]"
     }
 }
