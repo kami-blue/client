6b4b1f5ee0a1fd43a95d7dcb4e020f771bd65ff2
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-27 02:40:32 -0400

    Replaced deprecated MODULE_MANAGER in MixinEntityPlayerSP

--- src/main/java/me/zeroeightsix/kami/mixin/client/MixinEntityPlayerSP.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/MixinEntityPlayerSP.java
@@ -38,13 +38,13 @@ public abstract class MixinEntityPlayerSP extends EntityPlayer {
     @SuppressWarnings("UnnecessaryReturnStatement")
     @Redirect(method = "onLivingUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/entity/EntityPlayerSP;closeScreen()V"))
     public void closeScreen(EntityPlayerSP entityPlayerSP) {
-        if (MODULE_MANAGER.isModuleEnabled(PortalChat.class)) return;
+        if (ModuleManager.isModuleEnabled(PortalChat.class)) return;
     }
 
     @SuppressWarnings("UnnecessaryReturnStatement")
     @Redirect(method = "onLivingUpdate", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;displayGuiScreen(Lnet/minecraft/client/gui/GuiScreen;)V"))
     public void closeScreen(Minecraft minecraft, GuiScreen screen) {
-        if (MODULE_MANAGER.isModuleEnabled(PortalChat.class)) return;
+        if (ModuleManager.isModuleEnabled(PortalChat.class)) return;
     }
 
     /**
@@ -53,7 +53,7 @@ public abstract class MixinEntityPlayerSP extends EntityPlayer {
      */
     @Inject(method = "displayGUIChest", at = @At("HEAD"), cancellable = true)
     public void onDisplayGUIChest(IInventory chestInventory, CallbackInfo ci) {
-        if (MODULE_MANAGER.isModuleEnabled(BeaconSelector.class)) {
+        if (ModuleManager.isModuleEnabled(BeaconSelector.class)) {
             if (chestInventory instanceof IInteractionObject) {
                 if ("minecraft:beacon".equals(((IInteractionObject) chestInventory).getGuiID())) {
                     Minecraft.getMinecraft().displayGuiScreen(new KamiGuiBeacon(this.inventory, chestInventory));
