3722290f76e86538f18ecd76f86b76a3bbb37a6a
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-27 02:28:01 -0400

    Added comment and annotations to AntiBot

--- src/main/java/me/zeroeightsix/kami/module/modules/combat/AntiBot.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/combat/AntiBot.kt
@@ -2,5 +2,16 @@ package me.zeroeightsix.kami.module.modules.combat
 
 import me.zeroeightsix.kami.module.Module
 
+/**
+ * @author Xiaro
+ *
+ * Created by Xiaro on 07/08/20
+ */
+@Module.Info(
+        name = "AntiBot",
+        description = "Avoid attacking fake entity",
+        category = Module.Category.COMBAT
+)
 class AntiBot:Module() {
+
 }
