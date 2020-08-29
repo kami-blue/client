ec08d2e45bcffda22312d020b59093341d23ac71
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-08-29 02:05:23 -0400

    Make Aura isActive() returns something Don't run aura if it other higher priority module is running

--- src/main/java/me/zeroeightsix/kami/module/modules/combat/Aura.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/combat/Aura.kt
@@ -1,9 +1,6 @@
 package me.zeroeightsix.kami.module.modules.combat
 
-import me.zero.alpine.listener.EventHandler
-import me.zero.alpine.listener.EventHook
-import me.zero.alpine.listener.Listener
-import me.zeroeightsix.kami.event.events.PacketEvent
+import me.zeroeightsix.kami.manager.mangers.PlayerPacketManager
 import me.zeroeightsix.kami.module.Module
 import me.zeroeightsix.kami.manager.mangers.CombatManager
 import me.zeroeightsix.kami.module.modules.misc.AutoTool.Companion.equipBestWeapon
@@ -11,15 +8,13 @@ import me.zeroeightsix.kami.setting.Setting
 import me.zeroeightsix.kami.setting.Settings
 import me.zeroeightsix.kami.util.BaritoneUtils.pause
 import me.zeroeightsix.kami.util.BaritoneUtils.unpause
-import me.zeroeightsix.kami.util.EntityUtils.EntityPriority
-import me.zeroeightsix.kami.util.EntityUtils.getPrioritizedTarget
-import me.zeroeightsix.kami.util.EntityUtils.getTargetList
 import me.zeroeightsix.kami.util.LagCompensator
+import me.zeroeightsix.kami.util.math.RotationUtils
 import me.zeroeightsix.kami.util.math.RotationUtils.faceEntity
 import me.zeroeightsix.kami.util.math.RotationUtils.getRotationToEntity
+import me.zeroeightsix.kami.util.math.Vec2f
 import net.minecraft.entity.Entity
 import net.minecraft.init.Items
-import net.minecraft.network.play.client.CPacketPlayer
 import net.minecraft.util.EnumHand
 
 /**
@@ -27,17 +22,17 @@ import net.minecraft.util.EnumHand
  * Updated by hub on 31 October 2019
  * Updated by bot-debug on 10/04/20
  * Baritone compat added by dominikaaaa on 18/05/20
- * Updated by Xiaro on 02/08/20
+ * Updated by Xiaro on 29/08/20
  */
 @Module.Info(
         name = "Aura",
         category = Module.Category.COMBAT,
         description = "Hits entities around you",
-        modulePriority = 2
+        modulePriority = 50
 )
 class Aura : Module() {
     private val delayMode = register(Settings.e<WaitMode>("Mode", WaitMode.DELAY))
-    private val multi = register(Settings.b("Multi", true))
+    private val multi = register(Settings.b("Multi", false))
     private val spoofRotation = register(Settings.booleanBuilder("SpoofRotation").withValue(true).withVisibility { !multi.value }.build())
     private val lockView = register(Settings.booleanBuilder("LockView").withValue(false).withVisibility { !multi.value }.build())
     private val waitTick = register(Settings.floatBuilder("SpamDelay").withMinimum(0.1f).withValue(2.0f).withMaximum(40.0f).withVisibility { delayMode.value == WaitMode.SPAM }.build())
@@ -50,8 +45,6 @@ class Aura : Module() {
     private val disableOnDeath = register(Settings.b("DisableOnDeath", false))
 
     private var startTime: Long = 0
-    private var yaw = 0f
-    private var pitch = 0f
     private var tickCount = 0
     var active = false // returned to AutoEat
 
@@ -63,25 +56,17 @@ class Aura : Module() {
         SWORD, AXE, NONE
     }
 
-    @EventHandler
-    private val sendListener = Listener(EventHook { event: PacketEvent.Send ->
-        if (mc.player == null || !spoofRotation.value || !active || event.packet !is CPacketPlayer) return@EventHook
-        val packet = event.packet as CPacketPlayer
-        packet.yaw = yaw
-        packet.pitch = pitch
-    })
+    override fun isActive(): Boolean {
+        return active && isEnabled
+    }
 
     override fun onUpdate() {
+        if (CombatManager.getTopModule() !is Aura) return
         if (mc.player.isDead) {
             if (mc.player.isDead && disableOnDeath.value) disable()
             return
         }
-        if (spoofRotation.value) {
-            mc.player.rotationYaw += Math.random().toFloat() * 0.005f - 0.0025f
-            mc.player.rotationPitch += Math.random().toFloat() * 0.005f - 0.0025f
-        }
         if (autoTool.value) equipBestWeapon(prefer.value)
-        CombatManager.moduleList[this.javaClass] = active
         if (multi.value) {
             val targetList = CombatManager.targetList
             if (targetList.isEmpty()) {
@@ -99,8 +84,10 @@ class Aura : Module() {
             }
             if (spoofRotation.value) {
                 val rotation = getRotationToEntity(target)
-                yaw = rotation.first.toFloat()
-                pitch = rotation.second.toFloat()
+                val yaw = rotation.first.toFloat()
+                val pitch = rotation.second.toFloat()
+                val packet = PlayerPacketManager.PlayerPacket(rotating = true, rotation = Vec2f(yaw, pitch))
+                PlayerPacketManager.addPacket(this, packet)
             }
             if (lockView.value) faceEntity(target)
             if (canAttack()) attack(target)
