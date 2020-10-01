749caf83cb2404659ffeacfa6e5b64dac53cbe35
Dominika <sokolov.dominika@gmail.com>
2020-09-30 21:25:33 -0400

    Refactor filters into separate file
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/module/modules/chat/AntiSpam.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/chat/AntiSpam.kt
@@ -9,6 +9,7 @@ import me.zeroeightsix.kami.setting.Setting
 import me.zeroeightsix.kami.setting.Settings
 import me.zeroeightsix.kami.util.text.MessageDetectionHelper
 import me.zeroeightsix.kami.util.text.MessageSendHelper
+import me.zeroeightsix.kami.util.text.SpamFilters
 import net.minecraft.util.text.TextComponentString
 import net.minecraftforge.client.event.ClientChatReceivedEvent
 import java.util.*
@@ -25,8 +26,7 @@ import java.util.stream.Collectors
 )
 object AntiSpam : Module() {
     private val mode = register(Settings.e<Mode>("Mode", Mode.REPLACE))
-    private val replaceMode = register(Settings.enumBuilder(ReplaceMode::class.java).withName("ReplaceMode")
-            .withValue(ReplaceMode.ASTERISKS).withVisibility { mode.value == Mode.REPLACE }.build())
+    private val replaceMode = register(Settings.enumBuilder(ReplaceMode::class.java).withName("ReplaceMode").withValue(ReplaceMode.ASTERISKS).withVisibility { mode.value == Mode.REPLACE }.build())
     private val p = register(Settings.e<Page>("Page", Page.TYPE))
 
     /* Page One */
@@ -43,13 +43,11 @@ object AntiSpam : Module() {
     /* Page Two */
     private val aggressiveFiltering = register(Settings.booleanBuilder("AggressiveFiltering").withValue(true).withVisibility { p.value == Page.SETTINGS }.build())
     private val duplicates = register(Settings.booleanBuilder("Duplicates").withValue(true).withVisibility { p.value == Page.SETTINGS }.build())
-    private val duplicatesTimeout = register(Settings.integerBuilder("DuplicatesTimeout").withMinimum(1).withValue(30).withMaximum(600)
-            .withVisibility { duplicates.value && p.value == Page.SETTINGS }.build())
+    private val duplicatesTimeout = register(Settings.integerBuilder("DuplicatesTimeout").withMinimum(1).withValue(30).withMaximum(600).withVisibility { duplicates.value && p.value == Page.SETTINGS }.build())
     private val filterOwn = register(Settings.booleanBuilder("FilterOwn").withValue(false).withVisibility { p.value == Page.SETTINGS }.build())
     private val filterDMs = register(Settings.booleanBuilder("FilterDMs").withValue(false).withVisibility { p.value == Page.SETTINGS }.build())
     private val filterServer = register(Settings.booleanBuilder("FilterServer").withValue(false).withVisibility { p.value == Page.SETTINGS }.build())
-    private val showBlocked = register(Settings.enumBuilder(ShowBlocked::class.java).withName("ShowBlocked")
-            .withValue(ShowBlocked.LOG_FILE).withVisibility { p.value == Page.SETTINGS }.build())
+    private val showBlocked = register(Settings.enumBuilder(ShowBlocked::class.java).withName("ShowBlocked").withValue(ShowBlocked.LOG_FILE).withVisibility { p.value == Page.SETTINGS }.build())
 
     private var messageHistory: ConcurrentHashMap<String, Long>? = null
 
@@ -57,6 +55,7 @@ object AntiSpam : Module() {
         REPLACE, HIDE
     }
 
+    @Suppress("unused")
     private enum class ReplaceMode(val redaction: String) {
         REDACTED("[redacted]"), ASTERISKS("****")
     }
@@ -65,6 +64,7 @@ object AntiSpam : Module() {
         TYPE, SETTINGS
     }
 
+    @Suppress("unused")
     private enum class ShowBlocked {
         NONE, LOG_FILE, CHAT, BOTH
     }
@@ -74,16 +74,15 @@ object AntiSpam : Module() {
         if (mc.player == null) return@EventHook
 
         /* leijurv's sexy lambda to remove older entries in messageHistory */
-        messageHistory!!.entries
-                .stream()
-                .filter { entry: Map.Entry<String, Long> -> entry.value < System.currentTimeMillis() - 10 * 60 * 1000 } // 10 is delay in minutes
-                .collect(Collectors.toList())
-                .forEach(Consumer { entry: Map.Entry<String, Long> -> messageHistory!!.remove(entry.key) })
+        messageHistory?.let {
+            it.entries.stream()
+                    .filter { entry: Map.Entry<String, Long> -> entry.value < System.currentTimeMillis() - 600000 } // 10 minutes delay
+                    .collect(Collectors.toList())
+                    .forEach(Consumer { entry: Map.Entry<String, Long> -> it.remove(entry.key) })
+        }
 
-        if (duplicates.value) {
-            if (checkDupes(event.message.unformattedText)) {
-                event.isCanceled = true
-            }
+        if (duplicates.value && checkDupes(event.message.unformattedText)) {
+            event.isCanceled = true
         }
 
         val pattern = isSpam(event.message.unformattedText)
@@ -96,7 +95,7 @@ object AntiSpam : Module() {
         }
 
         if (fancyChat.value) {
-            var message = sanitizeFancyChat(event.message.unformattedText)
+            val message = sanitizeFancyChat(event.message.unformattedText)
             if (message.trim { it <= ' ' }.isEmpty()) { // this should be removed if we are going for an intelligent de-fancy
                 event.message = TextComponentString(getUsername(event.message.unformattedText) + " [Fancychat]")
             }
@@ -187,175 +186,27 @@ object AntiSpam : Module() {
     }
 
     private fun sanitizeFancyChat(toClean: String): String {
-        return toClean.replace("[^\\u0000-\\u007F]".toRegex(), "") // this has the potential to be intelligent and convert to ascii instead of just delete
+        // this has the potential to be intelligent and convert to ascii instead of just delete
+        return toClean.replace("[^\\u0000-\\u007F]".toRegex(), "")
     }
 
 
     private val settingMap: HashMap<Setting<Boolean>, Array<String>> = object : HashMap<Setting<Boolean>, Array<String>>() {
         init {
-            put(greenText, FilterPatterns.GREEN_TEXT)
-            put(specialCharBegin, FilterPatterns.SPECIAL_BEGINNING)
-            put(specialCharEnding, FilterPatterns.SPECIAL_ENDING)
-            put(automated, FilterPatterns.OWNS_ME_AND_ALL)
-            put(automated, FilterPatterns.I_JUST_THANKS_TO)
-            put(discordLinks, FilterPatterns.DISCORD)
-            put(ips, FilterPatterns.IP_ADDR)
-            put(automated, FilterPatterns.ANNOUNCER)
-            put(automated, FilterPatterns.SPAMMER)
-            put(automated, FilterPatterns.INSULTER)
-            put(automated, FilterPatterns.GREETER)
-            put(slurs, FilterPatterns.SLURS)
-            put(swears, FilterPatterns.SWEARS)
+            put(greenText, SpamFilters.greenText)
+            put(specialCharBegin, SpamFilters.specialBeginning)
+            put(specialCharEnding, SpamFilters.specialEnding)
+            put(automated, SpamFilters.ownsMeAndAll)
+            put(automated, SpamFilters.thanksTo)
+            put(discordLinks, SpamFilters.discordInvite)
+            put(ips, SpamFilters.ipAddress)
+            put(automated, SpamFilters.announcer)
+            put(automated, SpamFilters.spammer)
+            put(automated, SpamFilters.insulter)
+            put(automated, SpamFilters.greeter)
+            put(slurs, SpamFilters.slurs)
+            put(swears, SpamFilters.swears)
         }
-    }
-
-    private object FilterPatterns {
-        val ANNOUNCER = arrayOf( // RusherHack b8
-                "I just walked .+ feet!",
-                "I just placed a .+!",
-                "I just attacked .+ with a .+!",
-                "I just dropped a .+!",
-                "I just opened chat!",
-                "I just opened my console!",
-                "I just opened my GUI!",
-                "I just went into full screen mode!",
-                "I just paused my game!",
-                "I just opened my inventory!",
-                "I just looked at the player list!",
-                "I just took a screen shot!",
-                "I just swaped hands!",
-                "I just ducked!",
-                "I just changed perspectives!",
-                "I just jumped!",
-                "I just ate a .+!",
-                "I just crafted .+ .+!",
-                "I just picked up a .+!",
-                "I just smelted .+ .+!",
-                "I just respawned!",  // RusherHack b11
-                "I just attacked .+ with my hands",
-                "I just broke a .+!",  // WWE
-                "I recently walked .+ blocks",
-                "I just droped a .+ called, .+!",
-                "I just placed a block called, .+!",
-                "Im currently breaking a block called, .+!",
-                "I just broke a block called, .+!",
-                "I just opened chat!",
-                "I just opened chat and typed a slash!",
-                "I just paused my game!",
-                "I just opened my inventory!",
-                "I just looked at the player list!",
-                "I just changed perspectives, now im in .+!",
-                "I just crouched!",
-                "I just jumped!",
-                "I just attacked a entity called, .+ with a .+",
-                "Im currently eatting a peice of food called, .+!",
-                "Im currently using a item called, .+!",
-                "I just toggled full screen mode!",
-                "I just took a screen shot!",
-                "I just swaped hands and now theres a .+ in my main hand and a .+ in my off hand!",
-                "I just used pick block on a block called, .+!",
-                "Ra just completed his blazing ark",
-                "Its a new day yes it is",  // DotGod.CC
-                "I just placed .+ thanks to (http:\\/\\/)?DotGod\\.CC!",
-                "I just flew .+ meters like a butterfly thanks to (http:\\/\\/)?DotGod\\.CC!")
-        val SPAMMER = arrayOf( //WWE
-                "WWE Client's spammer",
-                "Lol get gud",
-                "Future client is bad",
-                "WWE > Future",
-                "WWE > Impact",
-                "Default Message",
-                "IKnowImEZ is a god",
-                "THEREALWWEFAN231 is a god",
-                "WWE Client made by IKnowImEZ/THEREALWWEFAN231",
-                "WWE Client was the first public client to have Path Finder/New Chunks",
-                "WWE Client was the first public client to have color signs",
-                "WWE Client was the first client to have Teleport Finder",
-                "WWE Client was the first client to have Tunneller & Tunneller Back Fill",
-                "Zispanos")
-        val INSULTER = arrayOf( // WWE
-                ".+ Download WWE utility mod, Its free!",
-                ".+ 4b4t is da best mintscreft serber",
-                ".+ dont abouse",
-                ".+ you cuck",
-                ".+ https://www.youtube.com/channel/UCJGCNPEjvsCn0FKw3zso0TA",
-                ".+ is my step dad",
-                ".+ again daddy!",
-                "dont worry .+ it happens to every one",
-                ".+ dont buy future it's crap, compared to WWE!",
-                "What are you, fucking gay, .+?",
-                "Did you know? .+ hates you, .+",
-                "You are literally 10, .+",
-                ".+ finally lost their virginity, sadly they lost it to .+... yeah, that's unfortunate.",
-                ".+, don't be upset, it's not like anyone cares about you, fag.",
-                ".+, see that rubbish bin over there? Get your ass in it, or I'll get .+ to whoop your ass.",
-                ".+, may I borrow that dirt block? that guy named .+ needs it...",
-                "Yo, .+, btfo you virgin",
-                "Hey .+ want to play some High School RP with me and .+?",
-                ".+ is an Archon player. Why is he on here? Fucking factions player.",
-                "Did you know? .+ just joined The Vortex Coalition!",
-                ".+ has successfully conducted the cactus dupe and duped a itemhand!",
-                ".+, are you even human? You act like my dog, holy shit.",
-                ".+, you were never loved by your family.",
-                "Come on .+, you hurt .+'s feelings. You meany.",
-                "Stop trying to meme .+, you can't do that. kek",
-                ".+, .+ is gay. Don't go near him.",
-                "Whoa .+ didn't mean to offend you, .+.",
-                ".+ im not pvping .+, im WWE'ing .+.",
-                "Did you know? .+ just joined The Vortex Coalition!",
-                ".+, are you even human? You act like my dog, holy shit.")
-        val GREETER = arrayOf( // WWE
-                "Bye, Bye .+",
-                "Farwell, .+",  // Others(?)
-                "See you next time, .+",
-                "Catch ya later, .+",
-                "Bye, .+",
-                "Welcome, .+",
-                "Hey, .+",  // Vanilla MC / Essentials MC
-                ".+ joined the game",
-                ".+ has joined",
-                ".+ joined the lobby",
-                "Welcome .+",
-                ".+ left the game")
-        val DISCORD = arrayOf(
-                "discord.gg",
-                "discordapp.com",
-                "discord.io",
-                "invite.gg")
-        val GREEN_TEXT = arrayOf(
-                "^>.+$")
-        val IP_ADDR = arrayOf(
-                "\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\:\\d{1,5}\\b",
-                "\\b\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}",
-                "^(?:http(?:s)?:\\/\\/)?(?:[^\\.]+\\.)?.*\\..*\\..*$",
-                ".*\\..*\\:\\d{1,5}$")
-        val OWNS_ME_AND_ALL = arrayOf(
-                "owns me and all")
-        val I_JUST_THANKS_TO = arrayOf(
-                "i just.*thanks to",
-                "i just.*using")
-        val SPECIAL_BEGINNING = arrayOf(
-                "^[.,/?!()\\[\\]{}<|\\-+=\\\\]")
-        val SPECIAL_ENDING = arrayOf(
-                "[/@#^()\\[\\]{}<>|\\-+=\\\\]$")
-        val SLURS = arrayOf(
-                "nigg.{0,3}",
-                "chi[^c]k",
-                "tra.{0,1}n(y|ie)",
-                "kik.{1,2}",
-                "fa(g |g.{0,2})",
-                "reta.{0,3}"
-        )
-
-        // fairly simple - anarchy servers shouldn't have a chat filter so there isn't an attempt to check for bypasses
-        val SWEARS = arrayOf(
-                "fuck(er)?",
-                "shit",
-                "cunt",
-                "puss(ie|y)",
-                "bitch",
-                "twat"
-        )
     }
 
     private fun sendResult(name: String, message: String) {
