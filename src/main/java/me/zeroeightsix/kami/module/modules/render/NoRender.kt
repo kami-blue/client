36a9010c5e3dca0ed5274f64440ca555ff0fcf2d
FriedrichM <friedrich.mueller@udo.edu>
2020-10-20 11:50:02 +0200

    noRender for Enchantingtables

--- src/main/java/me/zeroeightsix/kami/module/modules/render/NoRender.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/render/NoRender.kt
@@ -1,12 +1,16 @@
 package me.zeroeightsix.kami.module.modules.render
 
+import me.zeroeightsix.kami.event.events.ChunkEvent
 import me.zeroeightsix.kami.event.events.PacketEvent
 import me.zeroeightsix.kami.event.events.SafeTickEvent
 import me.zeroeightsix.kami.module.Module
 import me.zeroeightsix.kami.setting.Settings
 import me.zeroeightsix.kami.util.event.listener
+import net.minecraft.block.BlockSnow
 import net.minecraft.entity.item.EntityItem
+import net.minecraft.init.Blocks
 import net.minecraft.network.play.server.*
+import net.minecraft.util.math.BlockPos
 import net.minecraftforge.client.event.RenderBlockOverlayEvent
 
 @Module.Info(
@@ -27,6 +31,8 @@ object NoRender : Module() {
     val beacon = register(Settings.b("BeaconBeams", false))
     val skylight = register(Settings.b("SkyLightUpdates", true))
     val particles = register(Settings.b("Particles", false))
+    val enchantingtable = register(Settings.b("Enchantingtable Books", true))
+    val enchantingtableSnow = register(Settings.b("Enchantingtable as Snow", false))
 
     init {
         listener<PacketEvent.Receive> {
@@ -38,6 +44,21 @@ object NoRender : Module() {
                     it.packet is SPacketExplosion && explosion.value ||
                     it.packet is SPacketSpawnPainting && paint.value ||
                     it.packet is SPacketParticles && particles.value) it.cancel()
+        }
+
+        listener<ChunkEvent> {
+            if(!enchantingtableSnow.value)return@listener
+            val chunk =it.chunk
+            val layer =Blocks.SNOW_LAYER.defaultState.withProperty(BlockSnow.LAYERS, Integer.valueOf(7))
+            val yRange = IntRange(0, 256)
+            val xRange = IntRange(chunk.x*16, chunk.x*16+15)
+            val zRange = IntRange(chunk.z*16, chunk.z*16+15)
+
+            for (y in yRange) for (x in xRange) for (z in zRange) {
+                if (chunk.getBlockState(BlockPos(chunk.x * 16+x, y, chunk.z * 16+z)).block== Blocks.ENCHANTING_TABLE){
+                    chunk.setBlockState(BlockPos(chunk.x * 16+x, y, chunk.z * 16+z),layer )
+                }
+            }
         }
 
         listener<RenderBlockOverlayEvent> {
