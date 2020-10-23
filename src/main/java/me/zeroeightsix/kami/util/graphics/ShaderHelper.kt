780a40daf74efd5c8a2730d20b12fa06de2d8caa
Dominika <sokolov.dominika@gmail.com>
2020-10-22 20:45:18 -0400

    [fix] Properly detect shader support on Intel GPUs
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/util/graphics/ShaderHelper.kt
+++ src/main/java/me/zeroeightsix/kami/util/graphics/ShaderHelper.kt
@@ -1,16 +1,17 @@
 package me.zeroeightsix.kami.util.graphics
 
-
 import me.zeroeightsix.kami.KamiMod
 import me.zeroeightsix.kami.event.KamiEventBus
 import me.zeroeightsix.kami.event.events.ResolutionUpdateEvent
 import me.zeroeightsix.kami.util.Wrapper
 import me.zeroeightsix.kami.util.event.listener
+import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.client.renderer.OpenGlHelper
 import net.minecraft.client.shader.Framebuffer
 import net.minecraft.client.shader.ShaderGroup
 import net.minecraft.client.shader.ShaderLinkHelper
 import net.minecraft.util.ResourceLocation
+import org.lwjgl.opengl.GL11
 
 class ShaderHelper(shaderIn: ResourceLocation, vararg frameBufferNames: String) {
     private val mc = Wrapper.minecraft
@@ -19,14 +20,21 @@ class ShaderHelper(shaderIn: ResourceLocation, vararg frameBufferNames: String) 
     private val frameBufferMap = HashMap<String, Framebuffer>()
 
     init {
-        shader = if (OpenGlHelper.shadersSupported) {
+        shader = if (!OpenGlHelper.shadersSupported) {
+            KamiMod.log.warn("Shaders are unsupported by OpenGL!")
+            null
+        } else if (isIntegratedGraphics()) {
+            KamiMod.log.warn("Running on Intel Integrated Graphics!")
+            null
+        } else {
+            println("=\n".repeat(50) + GlStateManager.glGetString(GL11.GL_VENDOR))
             try {
                 ShaderLinkHelper.setNewStaticShaderLinkHelper()
                 ShaderGroup(mc.textureManager, mc.resourceManager, mc.framebuffer, shaderIn).also {
                     it.createBindFramebuffers(mc.displayWidth, mc.displayHeight)
                 }
             } catch (e: Exception) {
-                KamiMod.log.warn("Failed loading Shader")
+                KamiMod.log.warn("Failed to load shaders")
                 e.printStackTrace()
                 null
             }?.also {
@@ -34,8 +42,6 @@ class ShaderHelper(shaderIn: ResourceLocation, vararg frameBufferNames: String) 
                     frameBufferMap[name] = it.getFramebufferRaw(name)
                 }
             }
-        } else {
-            null
         }
     }
 
@@ -44,9 +50,18 @@ class ShaderHelper(shaderIn: ResourceLocation, vararg frameBufferNames: String) 
     // We are putting it here so it can find the listener above
     init {
         listener<ResolutionUpdateEvent> {
-            shader?.createBindFramebuffers(it.width, it.height)
+            shader?.createBindFramebuffers(it.width, it.height) // this will not run if on Intel GPU or unsupported Shaders
         }
 
         KamiEventBus.subscribe(this)
+    }
+
+    companion object {
+        private var cachedIsIntegratedGraphics: Boolean? = null
+
+        fun isIntegratedGraphics() = cachedIsIntegratedGraphics ?: run {
+            cachedIsIntegratedGraphics = GlStateManager.glGetString(GL11.GL_VENDOR).contains("Intel")
+            cachedIsIntegratedGraphics!! // cannot be null, we just set the value
+        }
     }
 }
