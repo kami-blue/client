d08b731459e902d6b5eb51d6af4d7263954ffd53
Dewy <dewy@dewy.dev>
2020-10-24 13:53:13 +0100

    remove 2nd init block and fixed  a n o t h e r  black screen bug

--- src/main/java/me/zeroeightsix/kami/util/graphics/ShaderHelper.kt
+++ src/main/java/me/zeroeightsix/kami/util/graphics/ShaderHelper.kt
@@ -11,6 +11,7 @@ import net.minecraft.client.shader.Framebuffer
 import net.minecraft.client.shader.ShaderGroup
 import net.minecraft.client.shader.ShaderLinkHelper
 import net.minecraft.util.ResourceLocation
+import net.minecraftforge.fml.common.gameevent.TickEvent
 import org.lwjgl.opengl.GL11
 
 class ShaderHelper(shaderIn: ResourceLocation, vararg frameBufferNames: String) {
@@ -19,22 +20,28 @@ class ShaderHelper(shaderIn: ResourceLocation, vararg frameBufferNames: String) 
     val shader: ShaderGroup?
     private val frameBufferMap = HashMap<String, Framebuffer>()
 
+    private var frameBuffersInitialized = false
+
     init {
         shader = if (!OpenGlHelper.shadersSupported) {
             KamiMod.log.warn("Shaders are unsupported by OpenGL!")
+
             null
         } else if (isIntegratedGraphics()) {
             KamiMod.log.warn("Running on Intel Integrated Graphics!")
+
             null
         } else {
             try {
                 ShaderLinkHelper.setNewStaticShaderLinkHelper()
+
                 ShaderGroup(mc.textureManager, mc.resourceManager, mc.framebuffer, shaderIn).also {
                     it.createBindFramebuffers(mc.displayWidth, mc.displayHeight)
                 }
             } catch (e: Exception) {
                 KamiMod.log.warn("Failed to load shaders")
                 e.printStackTrace()
+
                 null
             }?.also {
                 for (name in frameBufferNames) {
@@ -42,18 +49,23 @@ class ShaderHelper(shaderIn: ResourceLocation, vararg frameBufferNames: String) 
                 }
             }
         }
-    }
 
-    fun getFrameBuffer(name: String) = frameBufferMap[name]
+        listener<TickEvent.ClientTickEvent> {
+            if (!frameBuffersInitialized) {
+                shader?.createBindFramebuffers(mc.displayWidth, mc.displayHeight)
 
-    // We are putting it here so it can find the listener above
-    init {
+                frameBuffersInitialized = true
+            }
+        }
+
         listener<ResolutionUpdateEvent> {
             shader?.createBindFramebuffers(it.width, it.height) // this will not run if on Intel GPU or unsupported Shaders
         }
 
         KamiEventBus.subscribe(this)
     }
+
+    fun getFrameBuffer(name: String) = frameBufferMap[name]
 
     companion object {
         private var cachedIsIntegratedGraphics: Boolean? = null
