8304ef59330d5846d5f4179460cde6ca4f66b6a2
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-10-28 13:27:45 -0400

    Add more stuff to HudElement lol

--- src/main/java/me/zeroeightsix/kami/gui/hudgui/HudElement.kt
+++ src/main/java/me/zeroeightsix/kami/gui/hudgui/HudElement.kt
@@ -4,14 +4,47 @@ import me.zeroeightsix.kami.event.KamiEventBus
 import me.zeroeightsix.kami.gui.rgui.windows.BasicWindow
 
 open class HudElement(
-        name: String,
-        val description: String
+        name: String
 ) : BasicWindow(name, 20.0f, 20.0f, 100.0f, 50.0f, true) {
+
+    // Annotations
+    private val annotation =
+            javaClass.annotations.firstOrNull { it is Info } as? Info
+                    ?: throw IllegalStateException("No Annotation on class " + this.javaClass.canonicalName + "!")
+
+    val alias = arrayOf(name, *annotation.alias)
+    val category = annotation.category
+    val description = annotation.description
+    var alwaysListening = annotation.alwaysListening
+
+    annotation class Info(
+            val alias: Array<String> = [],
+            val description: String,
+            val category: Category,
+            val alwaysListening: Boolean = false
+    )
+
+    enum class Category(val displayName: String) {
+        COMBAT("Combat"),
+        PLAYER("Player"),
+        WORLD("World"),
+        MISC("Misc")
+    }
+    // End of annotations
+
+
+    override fun onGuiInit() {
+        super.onGuiInit()
+        if (alwaysListening) KamiEventBus.subscribe(this)
+    }
+
+    final override fun onTick() { super.onTick() }
+
 
     init {
         visible.valueListeners.add { _, it ->
             if (it) KamiEventBus.subscribe(this)
-            else KamiEventBus.unsubscribe(this)
+            else if (!alwaysListening) KamiEventBus.unsubscribe(this)
         }
     }
 
