11d95d26239fbcc1c6fe623c0412f6d1272bc746
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-10-29 15:31:42 -0400

    Added tps hud

---
+++ src/main/java/me/zeroeightsix/kami/gui/hudgui/elements/misc/TPS.kt
@@ -0,0 +1,27 @@
+package me.zeroeightsix.kami.gui.hudgui.elements.misc
+
+import me.zeroeightsix.kami.gui.hudgui.HudElement
+import me.zeroeightsix.kami.gui.hudgui.LabelHud
+import me.zeroeightsix.kami.util.TpsCalculator
+import me.zeroeightsix.kami.util.math.MathUtils
+
+@HudElement.Info(
+        category = HudElement.Category.MISC,
+        description = "Display server TPS"
+)
+object TPS : LabelHud("FPS") {
+
+    private val tpsList = FloatArray(100) { 20.0f }
+    private var tpsIndex = 0
+
+    override fun updateText() {
+        tpsList[tpsIndex] = TpsCalculator.tickRate
+        tpsIndex = (tpsIndex + 1) % 20
+
+        val tps = MathUtils.round(tpsList.average(), 2)
+
+        displayText.add("$tps")
+        displayText.add("tps")
+    }
+
+}
