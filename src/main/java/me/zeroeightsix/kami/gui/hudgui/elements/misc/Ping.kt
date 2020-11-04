7359589e260726b2042d0be1fd76a6bc57145feb
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-03 23:03:59 -0500

    Added ping hud

---
+++ src/main/java/me/zeroeightsix/kami/gui/hudgui/elements/misc/Ping.kt
@@ -0,0 +1,18 @@
+package me.zeroeightsix.kami.gui.hudgui.elements.misc
+
+import me.zeroeightsix.kami.gui.hudgui.HudElement
+import me.zeroeightsix.kami.gui.hudgui.LabelHud
+import me.zeroeightsix.kami.util.InfoCalculator
+
+@HudElement.Info(
+        category = HudElement.Category.MISC,
+        description = "Display ping"
+)
+object Ping : LabelHud("Ping") {
+
+    override fun updateText() {
+        displayText.add(InfoCalculator.ping().toString(), primaryColor.value)
+        displayText.add("ms", secondaryColor.value)
+    }
+
+}
