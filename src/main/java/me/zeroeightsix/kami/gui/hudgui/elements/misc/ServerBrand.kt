0a82ffb6e5ace0a218a13bcb0f97568aef923688
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-03 23:05:35 -0500

    Added ServerBrand hud

---
+++ src/main/java/me/zeroeightsix/kami/gui/hudgui/elements/misc/ServerBrand.kt
@@ -0,0 +1,17 @@
+package me.zeroeightsix.kami.gui.hudgui.elements.misc
+
+import me.zeroeightsix.kami.gui.hudgui.HudElement
+import me.zeroeightsix.kami.gui.hudgui.LabelHud
+
+@HudElement.Info(
+        category = HudElement.Category.MISC,
+        description = "Display brand of the server"
+)
+object ServerBrand : LabelHud("ServerBrand") {
+
+    override fun updateText() {
+        val serverBrand = mc.player?.serverBrand?: "Unknown"
+        displayText.add(serverBrand, primaryColor.value)
+    }
+
+}
