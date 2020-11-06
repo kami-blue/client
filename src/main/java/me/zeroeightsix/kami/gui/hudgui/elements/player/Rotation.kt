ae829ee0d073175bf895d3cf22433ecb00afe853
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-05 23:04:05 -0500

    Added Rotation hud

---
+++ src/main/java/me/zeroeightsix/kami/gui/hudgui/elements/player/Rotation.kt
@@ -0,0 +1,23 @@
+package me.zeroeightsix.kami.gui.hudgui.elements.player
+
+import me.zeroeightsix.kami.gui.hudgui.HudElement
+import me.zeroeightsix.kami.gui.hudgui.LabelHud
+import me.zeroeightsix.kami.util.math.MathUtils
+import me.zeroeightsix.kami.util.math.RotationUtils
+
+@HudElement.Info(
+        category = HudElement.Category.PLAYER,
+        description = "Display player rotation"
+)
+object Rotation : LabelHud("Rotation") {
+
+    override fun updateText() {
+        val yaw = MathUtils.round(RotationUtils.normalizeAngle(mc.player?.rotationYaw?: 0.0f), 1)
+        val pitch = MathUtils.round(mc.player?.rotationPitch?: 0.0f, 1)
+        displayText.add("Yaw", secondaryColor.value)
+        displayText.add(yaw.toString(), primaryColor.value)
+        displayText.add("Pitch", secondaryColor.value)
+        displayText.add(pitch.toString(), primaryColor.value)
+    }
+
+}
