8635fb5124c63e7f8b1a71ae92fa18f021b9e338
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-15 14:23:24 -0500

    [enhancement] Rewrote Capes to use the new cape-api

--- src/main/java/me/zeroeightsix/kami/module/modules/client/Capes.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/client/Capes.kt
@@ -1,12 +1,18 @@
 package me.zeroeightsix.kami.module.modules.client
 
-import CapeColor
-import CapeUser
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import getRequest
 import me.zeroeightsix.kami.KamiMod
+import me.zeroeightsix.kami.event.events.SafeTickEvent
 import me.zeroeightsix.kami.module.Module
+import me.zeroeightsix.kami.util.TimerUtils
+import me.zeroeightsix.kami.util.event.listener
+import org.kamiblue.capeapi.Cape
+import org.kamiblue.capeapi.CapeUser
+import org.kamiblue.commons.utils.ConnectionUtils
+import org.kamiblue.commons.utils.ThreadUtils
+import java.util.*
+import kotlin.collections.HashMap
 
 @Module.Info(
         name = "Capes",
@@ -16,27 +22,42 @@ import me.zeroeightsix.kami.module.Module
         enabledByDefault = true
 )
 object Capes : Module() {
-    val capeUsers = HashMap<String, CapeColor>() // This will have to be replaced to be <UUID, Cape> in the future, when Star support is added
+    val capeUsers: MutableMap<UUID, Cape> = Collections.synchronizedMap(HashMap<UUID, Cape>())
+    var isPremium = false; private set
 
-    public override fun onEnable() {
-        Thread {
-            try {
-                val response = getRequest(KamiMod.CAPES_JSON)
-                val capeUsersCache = Gson().fromJson<ArrayList<CapeUser>?>(response, object : TypeToken<List<CapeUser>>() {}.type)
+    private val timer = TimerUtils.TickTimer(TimerUtils.TimeUnit.MINUTES)
+    private val gson = Gson()
+    private val thread = Thread({ updateCapes() }, "Capes Update Thread")
 
-                capeUsersCache?.forEach { capeUser ->
-                    capeUser.capes.forEach { cape ->
-                        cape.playerUUID?.let { capeUsers[it] = cape.color }
-                    }
-                } ?: run {
-                    KamiMod.log.warn("$chatName Failed to parse / download capes!")
-                }
-
-                KamiMod.log.info("$chatName loaded ${capeUsers.size} capes!")
-            } catch (e: Exception) {
-                KamiMod.log.error("Failed to load capes!")
-                e.printStackTrace()
-            }
-        }.start()
+    override fun onEnable() {
+        ThreadUtils.submitTask(thread)
     }
+
+    init {
+        listener<SafeTickEvent> {
+            if (timer.tick(5L)) ThreadUtils.submitTask(thread)
+        }
+    }
+
+    private fun updateCapes() {
+        val rawJson = ConnectionUtils.requestRawJsonFrom(KamiMod.CAPES_JSON) {
+            KamiMod.log.warn("Failed requesting capes", it)
+        } ?: return
+
+        try {
+            val cacheList = gson.fromJson<ArrayList<CapeUser>>(rawJson, object : TypeToken<List<CapeUser>>() {}.type)
+            cacheList.forEach { capeUser ->
+                capeUser.capes.forEach { cape ->
+                    cape.playerUUID?.let {
+                        capeUsers[it] = cape
+                        isPremium = isPremium || mc.session.profile.id == it && capeUser.isPremium
+                    }
+                }
+            }
+            KamiMod.log.info("Capes loaded")
+        } catch (e: Exception) {
+            KamiMod.log.warn("Failed parsing capes")
+        }
+    }
+
 }
