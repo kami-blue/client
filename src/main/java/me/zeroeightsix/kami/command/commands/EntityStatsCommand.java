2e472ce773bdc42ec89c2b63b6e6834825af375a
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-17 18:15:39 -0500

    [refactor] Make ModOwner and EntityStatsCommand uses UUIDManager to look up UUID

--- src/main/java/me/zeroeightsix/kami/command/commands/EntityStatsCommand.java
+++ src/main/java/me/zeroeightsix/kami/command/commands/EntityStatsCommand.java
@@ -1,12 +1,11 @@
 package me.zeroeightsix.kami.command.commands;
 
 import me.zeroeightsix.kami.command.Command;
-import me.zeroeightsix.kami.util.EntityUtils;
+import me.zeroeightsix.kami.manager.managers.UUIDManager;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.AbstractHorse;
+import org.kamiblue.capeapi.PlayerProfile;
 import org.kamiblue.commons.utils.MathUtils;
-
-import java.util.Objects;
 
 import static me.zeroeightsix.kami.util.text.MessageSendHelper.sendChatMessage;
 import static me.zeroeightsix.kami.util.text.MessageSendHelper.sendErrorMessage;
@@ -33,10 +32,14 @@ public class EntityStatsCommand extends Command {
             double jump = MathUtils.INSTANCE.round(-0.1817584952 * Math.pow(horse.getHorseJumpStrength(), 3) + 3.689713992 * Math.pow(horse.getHorseJumpStrength(), 2) + 2.128599134 * horse.getHorseJumpStrength() - 0.343930367, 4);
             String ownerId = horse.getOwnerUniqueId() == null ? "Not tamed." : horse.getOwnerUniqueId().toString();
 
+            PlayerProfile ownerProfile = UUIDManager.INSTANCE.getByString(ownerId);
+            String ownerName = "";
+            if (ownerProfile != null) ownerName = ownerProfile.getName();
+
             String builder = "&6Entity Statistics:" + "\n&cMax Health: " + maxHealth +
                     "\n&cSpeed: " + speed +
                     "\n&cJump: " + jump +
-                    "\n&cOwner: " + Objects.requireNonNull(EntityUtils.getNameFromUUID(ownerId)).replace("\"", "");
+                    "\n&cOwner: " + ownerName;
             sendChatMessage(builder);
         } else if (mc.player.getRidingEntity() instanceof EntityLivingBase) {
             EntityLivingBase entity = (EntityLivingBase) mc.player.getRidingEntity();
