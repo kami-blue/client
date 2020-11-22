1343152d366b33b9ad82bb360de6e1f18b963244
Dominika <sokolov.dominika@gmail.com>
2020-11-22 15:45:36 -0500

    [fix] Finish buildNamed.sh
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- scripts/buildNamed.sh
+++ scripts/buildNamed.sh
@@ -18,8 +18,26 @@ cd "$KAMI_DIR" || {
   exit $?
 }
 
+rm -rf build/libs/* || {
+  echo "[buildNamed] Failed to remove existing files in 'build/libs/', exiting."
+  exit $?
+}
+
 chmod +x gradlew
 ./gradlew build || {
   echo "[buildNamed] Gradle build failed, exiting."
   exit $?
 }
+
+cd build/libs/ || {
+  echo "[buildNamed] Failed to cd into build/libs/"
+  exit $?
+}
+
+__named=$(find . -maxdepth 1 -not -name "*release*" | tail -n +2)
+__bad_named=$(find . -maxdepth 1 -name "*release*")
+
+rm -f "$__named"
+mv "$__bad_named" "$__named"
+
+echo "$__named"
