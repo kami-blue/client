8f6cda066e9e6ca344840ddf492688b88c46e3b0
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-11-23 23:25:22 -0500

    Re added access transformer for MapItemRenderer

--- src/main/java/me/zeroeightsix/kami/mixin/client/render/MixinMapItemRenderer.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/render/MixinMapItemRenderer.java
@@ -1,12 +1,14 @@
 package me.zeroeightsix.kami.mixin.client.render;
 
 import me.zeroeightsix.kami.module.modules.render.NoRender;
-import me.zeroeightsix.kami.util.Wrapper;
+import net.minecraft.client.gui.MapItemRenderer;
 import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.lib.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 /**
  * Idea from littlebroto1
@@ -14,11 +16,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(targets = "net.minecraft.client.gui.MapItemRenderer$Instance")
 public class MixinMapItemRenderer {
 
+    @Shadow @Final private ResourceLocation location;
+
     private final ResourceLocation kamiMap = new ResourceLocation("kamiblue/kamimap.png");
 
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;enableAlpha()V"))
-    public void render(boolean noOverlayRendering, CallbackInfo ci) {
-        if (NoRender.INSTANCE.isEnabled() && NoRender.INSTANCE.getMap().getValue()) Wrapper.getMinecraft().getTextureManager().bindTexture(kamiMap);
+    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/MapItemRenderer$Instance;location:Lnet/minecraft/util/ResourceLocation;", opcode = Opcodes.GETFIELD))
+    public ResourceLocation render(MapItemRenderer.Instance instance) {
+        if (NoRender.INSTANCE.isEnabled() && NoRender.INSTANCE.getMap().getValue()) return kamiMap;
+        else return this.location;
     }
-
 }
