5a73908d64b9b0590d21e23a5c4ef3ba2375dda6
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-10 09:30:00 -0500

    Draw normal rect for HudElement

--- src/main/java/me/zeroeightsix/kami/gui/hudgui/HudElement.kt
+++ src/main/java/me/zeroeightsix/kami/gui/hudgui/HudElement.kt
@@ -2,8 +2,11 @@ package me.zeroeightsix.kami.gui.hudgui
 
 import me.zeroeightsix.kami.event.KamiEventBus
 import me.zeroeightsix.kami.gui.rgui.windows.BasicWindow
+import me.zeroeightsix.kami.module.modules.client.GuiColors
 import me.zeroeightsix.kami.setting.GuiConfig
+import me.zeroeightsix.kami.util.graphics.RenderUtils2D
 import me.zeroeightsix.kami.util.graphics.VertexHelper
+import me.zeroeightsix.kami.util.math.Vec2d
 import me.zeroeightsix.kami.util.math.Vec2f
 import org.kamiblue.commons.interfaces.DisplayEnum
 
@@ -33,7 +36,8 @@ open class HudElement(
     }
 
     final override fun onRender(vertexHelper: VertexHelper, absolutePos: Vec2f) {
-        super.onRender(vertexHelper, absolutePos)
+        RenderUtils2D.drawRectFilled(vertexHelper, Vec2d(0.0, 0.0), Vec2d(renderWidth, renderHeight), GuiColors.backGround)
+        RenderUtils2D.drawRectOutline(vertexHelper, Vec2d(0.0, 0.0), Vec2d(renderWidth, renderHeight), 1.5f, GuiColors.outline)
         renderHud(vertexHelper)
     }
 
