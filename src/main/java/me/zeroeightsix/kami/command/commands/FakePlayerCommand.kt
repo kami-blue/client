19b54475654da8b8d55be2e920556bbe38da79c8
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-11 15:37:01 -0500

    Converted FakePlayerCommand to new command base

--- src/main/java/me/zeroeightsix/kami/command/commands/FakePlayerCommand.kt
+++ src/main/java/me/zeroeightsix/kami/command/commands/FakePlayerCommand.kt
@@ -1,14 +1,21 @@
 package me.zeroeightsix.kami.command.commands
 
+import me.zeroeightsix.kami.command.ClientCommand
 import me.zeroeightsix.kami.command.CommandOld
 import me.zeroeightsix.kami.module.modules.misc.FakePlayer
 import me.zeroeightsix.kami.util.text.MessageSendHelper
 
-class FakePlayerCommand : CommandOld("fakeplayer", null, "fp") {
-    override fun call(args: Array<out String?>) {
-        args[0]?.let {
-            FakePlayer.playerName.value = it
-            MessageSendHelper.sendChatMessage("${FakePlayer.name.value} player name has been set to $it")
-        } ?: MessageSendHelper.sendErrorMessage("Invalid name!")
+object FakePlayerCommand : ClientCommand(
+    name = "fakeplayer",
+    alias = arrayOf("fp"),
+) {
+    init {
+        string("name") { nameArg ->
+            execute {
+                val name = nameArg.value
+                FakePlayer.playerName.value = name
+                MessageSendHelper.sendChatMessage("${FakePlayer.name.value} player name has been set to $name")
+            }
+        }
     }
 }
