8a9facc061c98542003845cd3b15f7c93cbc7783
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-11 21:00:39 -0500

    Converted SayCommand to new command base

---
+++ src/main/java/me/zeroeightsix/kami/command/commands/SayCommand.kt
@@ -0,0 +1,21 @@
+package me.zeroeightsix.kami.command.commands
+
+import me.zeroeightsix.kami.command.ClientCommand
+import me.zeroeightsix.kami.util.text.MessageSendHelper.sendServerMessage
+import me.zeroeightsix.kami.command.CommandOld
+import me.zeroeightsix.kami.command.syntax.ChunkBuilder
+import me.zeroeightsix.kami.util.text.MessageSendHelper
+import java.lang.StringBuilder
+
+class SayCommand : ClientCommand(
+    name = "say",
+    description = "Allows you to send any message, even with a prefix in it."
+) {
+    init {
+        greedy("message") { messageArg ->
+            execute {
+                sendServerMessage(messageArg.value.trim())
+            }
+        }
+    }
+}
