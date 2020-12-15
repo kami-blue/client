fdfb0ed7e1f4c5217faac7dd9f06c14d3019ed07
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-14 19:51:14 -0500

    Converted PeekCommand to new command base

--- src/main/java/me/zeroeightsix/kami/command/commands/PeekCommand.kt
+++ src/main/java/me/zeroeightsix/kami/command/commands/PeekCommand.kt
@@ -1,8 +1,8 @@
 package me.zeroeightsix.kami.command.commands
 
-import me.zeroeightsix.kami.command.CommandOld
-import me.zeroeightsix.kami.command.syntax.SyntaxChunk
-import me.zeroeightsix.kami.util.Wrapper
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import me.zeroeightsix.kami.command.ClientCommand
 import me.zeroeightsix.kami.util.text.MessageSendHelper.sendChatMessage
 import net.minecraft.client.gui.ScaledResolution
 import net.minecraft.client.gui.inventory.GuiShulkerBox
@@ -10,38 +10,35 @@ import net.minecraft.item.ItemShulkerBox
 import net.minecraft.tileentity.TileEntityShulkerBox
 import java.util.*
 
-class PeekCommand : CommandOld("peek", SyntaxChunk.EMPTY) {
-    override fun call(args: Array<String>) {
-        Wrapper.world?.let { world ->
-            Wrapper.player?.let { player ->
-                val itemStack = player.inventory.getCurrentItem()
-                val item = itemStack.item
-                if (item is ItemShulkerBox) {
-                    val entityBox = TileEntityShulkerBox().apply {
-                        this.world = world
-                    }
-                    val nbtTag = itemStack.tagCompound ?: return
-                    entityBox.readFromNBT(nbtTag.getCompoundTag("BlockEntityTag"))
+object PeekCommand : ClientCommand(
+    name = "Peek",
+    alias = arrayOf("ShulkerPeek"),
+    description = "Look inside the contents of a shulker box without opening it."
+) {
+    init {
+        execute {
+            if (world == null || player == null) return@execute
 
-                    val scaledResolution = ScaledResolution(mc)
-                    val scaledWidth = scaledResolution.scaledWidth
-                    val scaledHeight = scaledResolution.scaledHeight
-                    val gui = GuiShulkerBox(player.inventory, entityBox)
-                    gui.setWorldAndResolution(mc, scaledWidth, scaledHeight)
-
-                    Timer().schedule(object : TimerTask() {
-                        override fun run() {
-                            mc.displayGuiScreen(gui)
-                        }
-                    }, 50L)
-                } else {
-                    sendChatMessage("You aren't carrying a shulker box.")
+            val itemStack = player.inventory.getCurrentItem()
+            val item = itemStack.item
+            if (item is ItemShulkerBox) {
+                val entityBox = TileEntityShulkerBox().apply {
+                    this.world = this@execute.world
                 }
+                val nbtTag = itemStack.tagCompound ?: return@execute
+                entityBox.readFromNBT(nbtTag.getCompoundTag("BlockEntityTag"))
+
+                val scaledResolution = ScaledResolution(mc)
+                GuiShulkerBox(player.inventory, entityBox)
+                    .setWorldAndResolution(mc, scaledResolution.scaledWidth, scaledResolution.scaledHeight)
+
+                commandScope.launch {
+                    delay(50L)
+                    mc.displayGuiScreen(GuiShulkerBox(player.inventory, entityBox))
+                }
+            } else {
+                sendChatMessage("You aren't carrying a shulker box.")
             }
         }
-    }
-
-    init {
-        setDescription("Look inside the contents of a shulker box without opening it")
     }
 }
