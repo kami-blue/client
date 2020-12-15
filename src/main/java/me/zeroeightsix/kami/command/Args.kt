89e4d871029a5130213da91d0096d2468655f06b
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-15 14:44:52 -0500

    Added new arg types

--- src/main/java/me/zeroeightsix/kami/command/Args.kt
+++ src/main/java/me/zeroeightsix/kami/command/Args.kt
@@ -1,9 +1,12 @@
 package me.zeroeightsix.kami.command
 
+import me.zeroeightsix.kami.manager.managers.UUIDManager
 import me.zeroeightsix.kami.module.Module
 import me.zeroeightsix.kami.module.ModuleManager
+import net.minecraft.block.Block
+import net.minecraft.item.Item
+import org.kamiblue.capeapi.PlayerProfile
 import org.kamiblue.command.AbstractArg
-
 
 class ModuleArg(
     override val name: String
@@ -11,6 +14,38 @@ class ModuleArg(
 
     override suspend fun convertToType(string: String?): Module? {
         return ModuleManager.getModuleOrNull(string)
+    }
+
+}
+
+class BlockArg(
+    override val name: String
+) : AbstractArg<Block>() {
+
+    override suspend fun convertToType(string: String?): Block? {
+        if (string == null) return null
+        return Block.getBlockFromName(string)
+    }
+
+}
+
+class ItemArg(
+    override val name: String
+) : AbstractArg<Item>() {
+
+    override suspend fun convertToType(string: String?): Item? {
+        if (string == null) return null
+        return Item.getByNameOrId(string)
+    }
+
+}
+
+class PlayerArg(
+    override val name: String
+) : AbstractArg<PlayerProfile>() {
+
+    override suspend fun convertToType(string: String?): PlayerProfile? {
+        return UUIDManager.getByString(string)
     }
 
 }
--- src/main/java/me/zeroeightsix/kami/command/ClientCommand.kt
+++ src/main/java/me/zeroeightsix/kami/command/ClientCommand.kt
@@ -6,6 +6,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import me.zeroeightsix.kami.module.Module
 import me.zeroeightsix.kami.util.Wrapper
+import net.minecraft.block.Block
+import net.minecraft.item.Item
+import org.kamiblue.capeapi.PlayerProfile
 import org.kamiblue.command.AbstractArg
 import org.kamiblue.command.CommandBuilder
 import org.kamiblue.command.utils.BuilderBlock
@@ -26,6 +29,30 @@ abstract class ClientCommand(
         block: BuilderBlock<Module>
     ) {
         arg(ModuleArg(name), block)
+    }
+
+    @CommandBuilder
+    protected fun AbstractArg<*>.block(
+        name: String,
+        block: BuilderBlock<Block>
+    ) {
+        arg(BlockArg(name), block)
+    }
+
+    @CommandBuilder
+    protected fun AbstractArg<*>.item(
+        name: String,
+        block: BuilderBlock<Item>
+    ) {
+        arg(ItemArg(name), block)
+    }
+
+    @CommandBuilder
+    protected fun AbstractArg<*>.player(
+        name: String,
+        block: BuilderBlock<PlayerProfile>
+    ) {
+        arg(PlayerArg(name), block)
     }
 
     @CommandBuilder
