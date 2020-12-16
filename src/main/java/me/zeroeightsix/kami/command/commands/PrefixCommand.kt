0208680c02f13a8bfd5ef1b9bdd92dee5b0bdfb3
Dominika <sokolov.dominika@gmail.com>
2020-12-15 22:39:56 -0500

    [refactor] Rewrite ;prefix
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

---
+++ src/main/java/me/zeroeightsix/kami/command/commands/PrefixCommand.kt
@@ -0,0 +1,32 @@
+package me.zeroeightsix.kami.command.commands
+
+import me.zeroeightsix.kami.command.ClientCommand
+import me.zeroeightsix.kami.command.CommandManager
+import me.zeroeightsix.kami.util.text.MessageSendHelper
+
+object PrefixCommand : ClientCommand(
+    name = "prefix",
+    description = "Change command prefix"
+) {
+    init {
+        literal("reset") {
+            execute("Reset the prefix to ;") {
+                CommandManager.prefix.value = ";"
+                MessageSendHelper.sendChatMessage("Reset prefix to [&7;&f]!")
+            }
+        }
+
+        string("new prefix") { prefixArg ->
+            execute("Set a new prefix") {
+                if (prefixArg.value.isEmpty() || prefixArg.value == "\\") {
+                    CommandManager.prefix.value = ";"
+                    MessageSendHelper.sendChatMessage("Reset prefix to [&7;&f]!")
+                    return@execute
+                }
+
+                CommandManager.prefix.value = prefixArg.value
+                MessageSendHelper.sendChatMessage("Set prefix to [&7${prefixArg.value}&f]!")
+            }
+        }
+    }
+}
