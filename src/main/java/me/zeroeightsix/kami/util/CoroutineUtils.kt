fb276246adfa2980e53196dd7c4bd66970125456
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-16 12:57:54 -0500

    Make onMainThread() throws exception on caller thread

--- src/main/java/me/zeroeightsix/kami/util/CoroutineUtils.kt
+++ src/main/java/me/zeroeightsix/kami/util/CoroutineUtils.kt
@@ -3,15 +3,30 @@ package me.zeroeightsix.kami.util
 import me.zeroeightsix.kami.command.ClientEvent
 import me.zeroeightsix.kami.command.SafeClientEvent
 import me.zeroeightsix.kami.command.toSafe
+import java.util.concurrent.Callable
 
 fun onMainThread(block: ClientEvent.() -> Unit) {
-    Wrapper.minecraft.addScheduledTask {
-        ClientEvent().block()
+    Wrapper.minecraft.addScheduledTask(Callable {
+        try {
+            ClientEvent().block()
+            null
+        } catch (e: Exception) {
+            e
+        }
+    }).get()?.let {
+        throw it
     }
 }
 
 fun onMainThreadSafe(block: SafeClientEvent.() -> Unit) {
-    Wrapper.minecraft.addScheduledTask {
-        ClientEvent().toSafe()?.block()
+    Wrapper.minecraft.addScheduledTask(Callable {
+        try {
+            ClientEvent().toSafe()?.block()
+            null
+        } catch (e: Exception) {
+            e
+        }
+    }).get()?.let {
+        throw it
     }
 }
