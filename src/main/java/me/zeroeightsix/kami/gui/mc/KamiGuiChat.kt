2827edc300d8ba5c999dbb1a3450a80d78d8676c
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-20 15:41:19 -0500

    Implemented argument auto complete

--- src/main/java/me/zeroeightsix/kami/gui/mc/KamiGuiChat.kt
+++ src/main/java/me/zeroeightsix/kami/gui/mc/KamiGuiChat.kt
@@ -1,5 +1,6 @@
 package me.zeroeightsix.kami.gui.mc
 
+import kotlinx.coroutines.launch
 import me.zeroeightsix.kami.command.CommandManager
 import me.zeroeightsix.kami.gui.kami.theme.kami.KamiGuiColors
 import me.zeroeightsix.kami.mixin.extension.historyBuffer
@@ -11,6 +12,7 @@ import me.zeroeightsix.kami.util.graphics.VertexHelper
 import me.zeroeightsix.kami.util.math.Vec2d
 import net.minecraft.client.gui.GuiChat
 import org.kamiblue.command.AbstractArg
+import org.kamiblue.command.AutoComplete
 import org.kamiblue.commons.extension.stream
 import org.lwjgl.input.Keyboard
 import java.util.*
@@ -28,7 +30,8 @@ class KamiGuiChat(
     }
 
     private var predictString = ""
-    private var currentArg: AbstractArg<*>? = null
+    private var cachePredict = ""
+    private var canAutoComplete = false
 
     override fun keyTyped(typedChar: Char, keyCode: Int) {
         if (guiChatKeyTyped(typedChar, keyCode)) return
@@ -38,11 +41,17 @@ class KamiGuiChat(
             return
         }
 
-        if (keyCode == Keyboard.KEY_TAB && predictString.isNotBlank()) {
+        if (canAutoComplete && keyCode == Keyboard.KEY_TAB && predictString.isNotBlank()) {
             inputField.text += "$predictString "
             predictString = ""
-        } else {
-            calcCommandPredict()
+        }
+
+        // Async offloading
+        CommandManager.commandScope.launch {
+            cachePredict = ""
+            canAutoComplete = false
+            autoComplete()
+            predictString = cachePredict
         }
     }
 
@@ -78,27 +87,74 @@ class KamiGuiChat(
         }
     }
 
-    private fun calcCommandPredict() {
-        predictString = ""
+    private suspend fun autoComplete() {
         val string = inputField.text.removePrefix(CommandManager.prefix)
-        val args = kotlin.runCatching { CommandManager.parseArguments(string) }.getOrNull() ?: return
-        var argCount = args.size
-        val inputName = args[0]
+        val parsedArgs = runCatching { CommandManager.parseArguments(string) }.getOrNull() ?: return
+        var argCount = parsedArgs.size
+        val inputName = parsedArgs[0]
 
+        // Run commandAutoComplete() and return if there are only one arg
         if (argCount == 1) {
-            CommandManager.getCommands()
-                .stream()
-                .flatMap { it.allNames.stream() }
-                .filter { it.length >= inputName.length && it.startsWith(inputName) }
-                .sorted()
-                .findFirst()
-                .orElse(null)
-                ?.let { predictString = it.substring(min(inputName.length, it.length)) }
-
+            commandAutoComplete(inputName)
             return
-        } else if (string.endsWith(' ') && string[min(string.length - 2, 0)] != ' ') {
+        }
+
+        // If the string ends with only one space (typing the next arg), adds 1 to the arg count
+        if (string.endsWith(' ') && !string.endsWith("  ")) {
             argCount += 1
         }
+
+        // Get available arg types for current arg index
+        val args = getArgTypeForAtIndex(parsedArgs, argCount - 1) ?: return
+
+        // Get the current input string
+        val inputString = parsedArgs.getOrNull(argCount - 1)
+
+        if (inputString == null) {
+            // If we haven't input anything yet, prints list of available arg types
+            if (args.isNotEmpty()) cachePredict = args.toSet().joinToString("/")
+            return
+        }
+
+        // Set cache predict to the first arg that impls AutoComplete
+        // And the auto complete result isn't null
+        for (arg in args) {
+            if (arg !is AutoComplete) continue
+            val result = arg.completeForInput(inputString)
+            if (result != null) {
+                cachePredict = result.substring(min(inputString.length, result.length))
+                canAutoComplete = true
+                break // Stop the iteration here because we get the non null result already
+            }
+        }
+    }
+
+    private fun commandAutoComplete(inputName: String) {
+        // Since we are doing multiple operation in a chain
+        // It would worth the payoff of using Stream
+        CommandManager.getCommands().stream()
+            .flatMap { it.allNames.stream() }
+            .filter { it.length >= inputName.length && it.startsWith(inputName) }
+            .sorted()
+            .findFirst()
+            .orElse(null)
+            ?.let {
+                cachePredict = it.substring(min(inputName.length, it.length))
+                canAutoComplete = true
+            }
+    }
+
+    private suspend fun getArgTypeForAtIndex(parsedArgs: Array<String>, argIndex: Int): List<AbstractArg<*>>? {
+        // Get the command for input name, map the arg trees to the count of match args
+        val command = CommandManager.getCommandOrNull(parsedArgs[0]) ?: return null
+        val treeMatchedCounts = command.finalArgs.map { it.countArgs(parsedArgs) to it }
+
+        // Get the max matched number of args, filter all trees that has less matched args
+        // And map to the current arg in the tree if exists
+        val maxMatches = treeMatchedCounts.maxOfOrNull { it.first } ?: return null
+        return treeMatchedCounts
+            .filter { it.first == maxMatches }
+            .mapNotNull { it.second.getArgTree().getOrNull(argIndex) }
     }
 
     override fun drawScreen(mouseX: Int, mouseY: Int, partialTicks: Float) {
