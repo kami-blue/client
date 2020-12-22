bde698b0aa5c3d0c5b901510b6c94d1330ba1daa
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-22 13:19:01 -0500

    Added plugin command

---
+++ src/main/java/me/zeroeightsix/kami/command/commands/PluginCommand.kt
@@ -0,0 +1,125 @@
+package me.zeroeightsix.kami.command.commands
+
+import me.zeroeightsix.kami.command.ClientCommand
+import me.zeroeightsix.kami.plugin.PluginLoader
+import me.zeroeightsix.kami.plugin.PluginManager
+import me.zeroeightsix.kami.util.text.MessageSendHelper
+import me.zeroeightsix.kami.util.text.formatValue
+import java.io.File
+
+object PluginCommand : ClientCommand(
+    name = "plugin",
+    description = "Manage plugins"
+) {
+    init {
+        literal("load") {
+            string("jar name") { nameArg ->
+                execute {
+                    val name = nameArg.value
+                    val file = File("${PluginManager.pluginPath}$name")
+                    if (!file.exists() || !file.extension.equals("jar", true)) {
+                        MessageSendHelper.sendErrorMessage("$name is not a valid jar file name!")
+                    }
+
+                    val time = System.currentTimeMillis()
+                    MessageSendHelper.sendChatMessage("Loading plugin $name...")
+
+                    val loader = PluginLoader(file)
+                    val plugin = loader.load()
+                    if (PluginManager.loadedPlugins.contains(plugin)) {
+                        MessageSendHelper.sendWarningMessage("Plugin $name already loaded!")
+                        return@execute
+                    }
+                    PluginManager.load(loader)
+
+                    val stopTime = System.currentTimeMillis() - time
+                    MessageSendHelper.sendChatMessage("Loaded plugin $name, took $stopTime ms!")
+                }
+            }
+        }
+
+        literal("reload") {
+            string("plugin name") { nameArg ->
+                execute {
+                    val name = nameArg.value
+                    val plugin = PluginManager.loadedPlugins[name]
+
+                    if (plugin == null) {
+                        MessageSendHelper.sendErrorMessage("No plugin found for name $name")
+                        return@execute
+                    }
+
+                    val time = System.currentTimeMillis()
+                    MessageSendHelper.sendChatMessage("Reloading plugins $name...")
+
+                    val file = PluginManager.pluginLoaderMap[plugin]!!.file
+                    PluginManager.unload(plugin)
+                    PluginManager.load(PluginLoader(file))
+
+                    val stopTime = System.currentTimeMillis() - time
+                    MessageSendHelper.sendChatMessage("Reloaded plugin $name, took $stopTime ms!")
+                }
+            }
+
+            execute {
+                val time = System.currentTimeMillis()
+                MessageSendHelper.sendChatMessage("Reloading plugins...")
+
+                PluginManager.unloadAll()
+                PluginManager.loadAll(PluginManager.preLoad())
+
+                val stopTime = System.currentTimeMillis() - time
+                MessageSendHelper.sendChatMessage("Reloaded plugins, took $stopTime ms!")
+            }
+        }
+
+        literal("unload") {
+            string("plugin name") { nameArg ->
+                execute {
+                    val name = nameArg.value
+                    val plugin = PluginManager.loadedPlugins[name]
+
+                    if (plugin == null) {
+                        MessageSendHelper.sendErrorMessage("No plugin found for name $name")
+                        return@execute
+                    }
+
+                    val time = System.currentTimeMillis()
+                    MessageSendHelper.sendChatMessage("Unloading plugin $name...")
+
+                    PluginManager.unload(plugin)
+
+                    val stopTime = System.currentTimeMillis() - time
+                    MessageSendHelper.sendChatMessage("Unloaded plugin $name, took $stopTime ms!")
+                }
+            }
+
+            execute {
+                val time = System.currentTimeMillis()
+                MessageSendHelper.sendChatMessage("Unloading plugins...")
+
+                PluginManager.unloadAll()
+
+                val stopTime = System.currentTimeMillis() - time
+                MessageSendHelper.sendChatMessage("Unloaded plugins, took $stopTime ms!")
+            }
+        }
+
+        literal("list") {
+            execute {
+                MessageSendHelper.sendChatMessage("Loaded plugins: ${formatValue(PluginManager.loadedPlugins.size)}")
+                if (PluginManager.loadedPlugins.isEmpty()) {
+                    MessageSendHelper.sendRawChatMessage("No plugin loaded")
+                } else {
+                    for ((index, plugin) in PluginManager.loadedPlugins.withIndex()) {
+                        MessageSendHelper.sendRawChatMessage("${formatValue(index)}. " +
+                            "Name: ${formatValue(plugin.name)}, " +
+                            "Version: ${formatValue(plugin.version)}, " +
+                            "Author: ${formatValue(plugin.author)}"
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
