559f2852d72b8d0e38f88df57f5d11385eb4c6db
Dominika <sokolov.dominika@gmail.com>
2020-12-29 21:36:08 -0500

    Fix import error
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/module/modules/player/FastBreak.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/player/FastBreak.kt
@@ -6,9 +6,8 @@ import me.zeroeightsix.kami.event.events.PacketEvent
 import me.zeroeightsix.kami.event.events.SafeTickEvent
 import me.zeroeightsix.kami.mixin.extension.blockHitDelay
 import me.zeroeightsix.kami.module.Module
-import me.zeroeightsix.kami.setting.Settings
-import me.zeroeightsix.kami.util.threads.defaultScope
 import me.zeroeightsix.kami.setting.ModuleConfig.setting
+import me.zeroeightsix.kami.util.threads.defaultScope
 import net.minecraft.network.play.client.CPacketPlayerDigging
 import org.kamiblue.event.listener.listener
 
