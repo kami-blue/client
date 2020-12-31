d93603d09823346da42c70e804f12daf2b538ee5
Xiaro <62033805+Xiaro@users.noreply.github.com>
2020-12-31 17:29:53 -0500

    Fixed tofus caused by PlayerModel

--- src/main/java/me/zeroeightsix/kami/gui/hudgui/elements/player/PlayerModel.kt
+++ src/main/java/me/zeroeightsix/kami/gui/hudgui/elements/player/PlayerModel.kt
@@ -2,24 +2,27 @@ package me.zeroeightsix.kami.gui.hudgui.elements.player
 
 import me.zeroeightsix.kami.gui.hudgui.HudElement
 import me.zeroeightsix.kami.setting.GuiConfig.setting
+import me.zeroeightsix.kami.util.graphics.GlStateUtils
 import me.zeroeightsix.kami.util.graphics.KamiTessellator
 import me.zeroeightsix.kami.util.graphics.VertexHelper
 import net.minecraft.client.gui.inventory.GuiInventory
+import net.minecraft.client.renderer.GlStateManager
 import net.minecraft.util.math.MathHelper
-import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL11.*
+import kotlin.math.roundToInt
 
 object PlayerModel : HudElement(
     name = "PlayerModel",
     category = Category.PLAYER,
     description = "Your player icon, or players you attacked"
 ) {
-    private val scale = setting("Size", 100, 10..200, 2)
-    private val resetDelay = setting("ResetDelay", 100, 0..200, 10)
-    private val emulatePitch = setting("EmulatePitch", true)
-    private val emulateYaw = setting("EmulateYaw", false)
+    private val scale by setting("Size", 1.0f, 0.5f..2.0f, 0.05f)
+    private val resetDelay by setting("ResetDelay", 100, 0..200, 5)
+    private val emulatePitch by setting("EmulatePitch", true)
+    private val emulateYaw by setting("EmulateYaw", false)
 
-    override val minWidth: Float get() = adjust(114f)
-    override val minHeight: Float get() = adjust(204f)
+    override val minWidth: Float get() = adjust(114.0f)
+    override val minHeight: Float get() = adjust(204.0f)
     override val resizable: Boolean = true
 
     override fun renderHud(vertexHelper: VertexHelper) {
@@ -27,22 +30,27 @@ object PlayerModel : HudElement(
         super.renderHud(vertexHelper)
 
         val attackedEntity = mc.player?.lastAttackedEntity
-        val entity = if (attackedEntity != null && mc.player.ticksExisted - mc.player.lastAttackedEntityTime <= resetDelay.value) {
+        val entity = if (attackedEntity != null && mc.player.ticksExisted - mc.player.lastAttackedEntityTime <= resetDelay) {
             attackedEntity
         } else {
             mc.player
         }
 
-        val yaw = if (emulateYaw.value) interpolateAndWrap(entity.prevRotationYaw, entity.rotationYaw) else 0.0f
-        val pitch = if (emulatePitch.value) interpolateAndWrap(entity.prevRotationPitch, entity.rotationPitch) else 0.0f
+        val yaw = if (emulateYaw) interpolateAndWrap(entity.prevRotationYaw, entity.rotationYaw) else 0.0f
+        val pitch = if (emulatePitch) interpolateAndWrap(entity.prevRotationPitch, entity.rotationPitch) else 0.0f
 
-        GL11.glTranslatef(width / 2, height - adjust(7.5f), 0f)
-        GuiInventory.drawEntityOnScreen(0, 0, scale.value, -yaw, -pitch, entity)
+        glPushMatrix()
+        glTranslatef(width / 2, height - adjust(7.5f), 0f)
+        GuiInventory.drawEntityOnScreen(0, 0, (scale * 35.0f).roundToInt(), -yaw, -pitch, entity)
+        GlStateUtils.texture2d(true)
+        GlStateUtils.blend(true)
+        GlStateManager.disableColorLogic()
+        glPopMatrix()
     }
 
     private fun interpolateAndWrap(prev: Float, current: Float): Float {
         return MathHelper.wrapDegrees(prev + (current - prev) * KamiTessellator.pTicks())
     }
 
-    private fun adjust(value: Float) = (value * scale.value) / 100f
+    private fun adjust(value: Float) = value * scale * 0.35f
 }
