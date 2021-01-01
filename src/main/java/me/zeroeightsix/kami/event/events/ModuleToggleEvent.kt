7f61a11b845221b91461732988dee6d73e43e94a
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-01-01 18:13:37 -0500

    Fixed build errors

--- src/main/java/me/zeroeightsix/kami/event/events/ModuleToggleEvent.kt
+++ src/main/java/me/zeroeightsix/kami/event/events/ModuleToggleEvent.kt
@@ -1,6 +1,6 @@
 package me.zeroeightsix.kami.event.events
 
-import me.zeroeightsix.kami.event.KamiEvent
+import me.zeroeightsix.kami.event.Event
 import me.zeroeightsix.kami.module.Module
 
-class ModuleToggleEvent(val module: Module, val prevState: Boolean) : KamiEvent()
+class ModuleToggleEvent(val module: Module, val prevState: Boolean) : Event
