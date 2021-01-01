704c9c92a1f3fcc8dab072f3924cb7481f25264e
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-01-01 17:40:52 -0500

    Fixed build error

--- src/main/java/me/zeroeightsix/kami/event/events/RenderEvent.kt
+++ src/main/java/me/zeroeightsix/kami/event/events/RenderEvent.kt
@@ -1,5 +1,5 @@
 package me.zeroeightsix.kami.event.events
 
-import me.zeroeightsix.kami.event.KamiEvent
+import me.zeroeightsix.kami.event.Event
 
-class RenderEvent : KamiEvent()
+class RenderEvent : Event
