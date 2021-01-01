0e9b34985801c56af52dd9302eeecb6ad1f55ac5
Dominika <sokolov.dominika@gmail.com>
2021-01-01 16:44:40 -0500

    [fix] method names
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/mixin/client/render/MixinRenderManager.java
+++ src/main/java/me/zeroeightsix/kami/mixin/client/render/MixinRenderManager.java
@@ -21,14 +21,14 @@ public class MixinRenderManager {
     }
 
     @Inject(method = "renderEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/Render;doRender(Lnet/minecraft/entity/Entity;DDDFF)V", shift = At.Shift.AFTER))
-    public void renderEntityPostShadow(Entity entity, double x, double y, double z, float yaw, float partialTicks, boolean debug, CallbackInfo ci) {
+    public void renderEntityPeri(Entity entity, double x, double y, double z, float yaw, float partialTicks, boolean debug, CallbackInfo ci) {
         if (entity == null) return;
         RenderEntityEvent event = new RenderEntityEvent(entity, x, y, z, yaw, partialTicks, Phase.PERI);
         KamiEventBus.INSTANCE.post(event);
     }
 
     @Inject(method = "renderEntity", at = @At("RETURN"))
-    public void renderEntityPostReturn(Entity entity, double x, double y, double z, float yaw, float partialTicks, boolean debug, CallbackInfo ci) {
+    public void renderEntityPost(Entity entity, double x, double y, double z, float yaw, float partialTicks, boolean debug, CallbackInfo ci) {
         if (entity == null) return;
         RenderEntityEvent event = new RenderEntityEvent(entity, x, y, z, yaw, partialTicks, Phase.POST);
         KamiEventBus.INSTANCE.post(event);
