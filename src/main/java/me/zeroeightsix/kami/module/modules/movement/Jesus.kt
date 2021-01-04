bd700ceaa3e86e1c48748d568196dc181e1d2102
Dominika <sokolov.dominika@gmail.com>
2021-01-04 12:13:26 -0500

    [cleanup] Make code easier to read
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/module/modules/movement/Jesus.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/movement/Jesus.kt
@@ -20,13 +20,15 @@ import org.kamiblue.commons.extension.floorToInt
 import org.kamiblue.event.listener.listener
 
 @Module.Info(
-        name = "Jesus",
-        description = "Allows you to walk on water",
-        category = Module.Category.MOVEMENT
+    name = "Jesus",
+    description = "Allows you to walk on water",
+    category = Module.Category.MOVEMENT
 )
 object Jesus : Module() {
 
-    private var dolphinSetting = register(Settings.b("dolphin", false))
+    private val dolphin = register(Settings.b("Dolphin", false))
+
+    private val WATER_WALK_AA = AxisAlignedBB(0.0, 0.0, 0.0, 1.0, 0.99, 1.0)
 
     override fun onToggle() {
         BaritoneUtils.settings?.assumeWalkOnWater?.value = isEnabled
@@ -34,30 +36,29 @@ object Jesus : Module() {
 
     init {
         safeListener<TickEvent.ClientTickEvent> {
-            if (dolphinSetting.value) {
-                if (isInWater(player) && !player.isSneaking) {
-                    player.motionY += 0.03999999910593033
-                }
-            } else {
-                if (isInWater(player) && !player.isSneaking) {
+            if (isInWater(player) && !player.isSneaking) {
+                if (dolphin.value) {
+                    player.motionY += 0.03999999910593033 // regular jump speed
+                } else {
                     player.motionY = 0.1
+
                     if (player.ridingEntity != null && player.ridingEntity !is EntityBoat) {
-                        player.ridingEntity!!.motionY = 0.3
+                        player.ridingEntity?.motionY = 0.3
                     }
                 }
             }
         }
 
         listener<AddCollisionBoxToListEvent> {
-            if (mc.player == null || mc.player.isSneaking || mc.player.fallDistance > 3 || dolphinSetting.value) return@listener
+            if (dolphin.value || mc.player == null || mc.player.isSneaking || mc.player.fallDistance > 3) return@listener
 
             if (
                 it.block is BlockLiquid
                 && it.entity is EntityBoat
+                && isAboveBlock(mc.player, it.pos)
                 && (isDrivenByPlayer(it.entity) || it.entity === mc.player)
                 && !isInWater(mc.player)
                 && (EntityUtils.isAboveWater(mc.player, false) || EntityUtils.isAboveWater(mc.player.ridingEntity, false))
-                && isAboveBlock(mc.player, it.pos)
             ) {
                 val axisAlignedBB = WATER_WALK_AA.offset(it.pos)
                 if (it.entityBox.intersects(axisAlignedBB)) it.collidingBoxes.add(axisAlignedBB)
@@ -66,7 +67,8 @@ object Jesus : Module() {
         }
 
         listener<PacketEvent.Send> {
-            if (dolphinSetting.value) return@listener
+            if (dolphin.value) return@listener
+
             if (it.packet is CPacketPlayer
                 && EntityUtils.isAboveWater(mc.player, true)
                 && !isInWater(mc.player)
@@ -77,8 +79,6 @@ object Jesus : Module() {
             }
         }
     }
-
-    private val WATER_WALK_AA = AxisAlignedBB(0.0, 0.0, 0.0, 1.0, 0.99, 1.0)
 
     private fun isAboveLand(entity: Entity): Boolean {
         val y = entity.posY - 0.01
@@ -98,7 +98,6 @@ object Jesus : Module() {
     }
 
     private fun isInWater(entity: Entity): Boolean {
-        if (entity == null) return false
         val y = (entity.posY + 0.01).floorToInt()
 
         for (x in entity.posX.floorToInt() until entity.posX.ceilToInt()) {
