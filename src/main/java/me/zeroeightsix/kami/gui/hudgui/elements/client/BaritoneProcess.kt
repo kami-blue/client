092b67df4e82c3ebd6d818ae0665950afd8a37a9
Dominika <sokolov.dominika@gmail.com>
2021-01-04 13:49:02 -0500

    Baritone too small
    Signed-off-by: Dominika <sokolov.dominika@gmail.com>

--- src/main/java/me/zeroeightsix/kami/gui/hudgui/elements/client/BaritoneProcess.kt
+++ src/main/java/me/zeroeightsix/kami/gui/hudgui/elements/client/BaritoneProcess.kt
@@ -11,6 +11,9 @@ object BaritoneProcess : LabelHud(
     description = "Shows what Baritone is doing"
 ) {
 
+    override val minWidth = 10f
+    override val minHeight = 10f
+
     override fun updateText() {
         val process = BaritoneUtils.primary?.pathingControlManager?.mostRecentInControl() ?: return
         if (!process.isPresent) return
