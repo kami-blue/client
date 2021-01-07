9f9d91d1ab60b43bea50135351c9d66b4211eeb9
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-01-06 20:56:27 -0500

    Added CachedValues

--- src/main/java/me/zeroeightsix/kami/command/Args.kt
+++ src/main/java/me/zeroeightsix/kami/command/Args.kt
@@ -1,15 +1,11 @@
 package me.zeroeightsix.kami.command
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import me.zeroeightsix.kami.manager.managers.UUIDManager
 import me.zeroeightsix.kami.module.Module
 import me.zeroeightsix.kami.module.ModuleManager
-import me.zeroeightsix.kami.util.BaritoneUtils
-import me.zeroeightsix.kami.util.TickTimer
-import me.zeroeightsix.kami.util.TimeUnit
-import me.zeroeightsix.kami.util.Wrapper
-import me.zeroeightsix.kami.util.threads.defaultScope
+import me.zeroeightsix.kami.util.*
+import me.zeroeightsix.kami.util.threads.runSafeR
 import net.minecraft.block.Block
 import net.minecraft.item.Item
 import net.minecraft.util.math.BlockPos
@@ -31,23 +27,12 @@ class ModuleArg(
     }
 
     private companion object {
-        val updateTimer = TickTimer(TimeUnit.SECONDS)
-        var cachedNames: Collection<String>? = null
-
-        val allAlias: Collection<String>
-            get() {
-                val cached = cachedNames
-
-                return if (cached == null || updateTimer.tick(5L)) {
-                    ModuleManager.getModules().stream()
-                        .flatMap { Arrays.stream(arrayOf(it.name, *it.alias)) }
-                        .sorted()
-                        .toList()
-                        .also { cachedNames = it }
-                } else {
-                    cached
-                }
-            }
+        val allAlias by CachedValue(5L, TimeUnit.SECONDS) {
+            ModuleManager.modules.stream()
+                .flatMap { Arrays.stream(arrayOf(it.name, *it.alias)) }
+                .sorted()
+                .toList()
+        }
     }
 
 }
@@ -130,22 +115,11 @@ class SchematicArg(
     }
 
     private companion object {
-        val timer = TickTimer(TimeUnit.SECONDS)
         val schematicFolder = File("schematics")
-        var cachedFiles = emptyList<String>()
 
-        val schematicFiles: Collection<String>
-            get() {
-                if (timer.tick(2L) && schematicFolder.isDirectory) {
-                    defaultScope.launch(Dispatchers.IO) {
-                        schematicFolder.listFiles()?.map { it.name }?.let {
-                            cachedFiles = it
-                        }
-                    }
-                }
-
-                return cachedFiles
-            }
+        val schematicFiles by AsyncCachedValue(5L, TimeUnit.SECONDS, Dispatchers.IO) {
+            schematicFolder.listFiles()?.map { it.name } ?: emptyList<String>()
+        }
     }
 }
 
@@ -179,14 +153,14 @@ class PlayerArg(
     }
 
     private companion object {
-        val playerInfoMap: Collection<String>?
-            get() {
-                val playerInfoMap = Wrapper.minecraft.connection?.playerInfoMap ?: return null
-                return playerInfoMap.stream()
+        val playerInfoMap by CachedValue(3L, TimeUnit.SECONDS) {
+            runSafeR {
+                connection.playerInfoMap.stream()
                     .map { it.gameProfile.name }
                     .sorted()
                     .toList()
             }
+        }
     }
 
 }
--- src/main/java/me/zeroeightsix/kami/module/ModuleManager.kt
+++ src/main/java/me/zeroeightsix/kami/module/ModuleManager.kt
@@ -3,14 +3,20 @@ package me.zeroeightsix.kami.module
 import kotlinx.coroutines.Deferred
 import me.zeroeightsix.kami.AsyncLoader
 import me.zeroeightsix.kami.KamiMod
+import me.zeroeightsix.kami.util.AsyncCachedValue
 import me.zeroeightsix.kami.util.StopTimer
+import me.zeroeightsix.kami.util.TimeUnit
 import org.kamiblue.commons.collections.AliasSet
 import org.kamiblue.commons.utils.ClassUtils
 import org.lwjgl.input.Keyboard
 
 object ModuleManager : AsyncLoader<List<Class<out Module>>> {
     override var deferred: Deferred<List<Class<out Module>>>? = null
+
     private val moduleSet = AliasSet<Module>()
+    val modules by AsyncCachedValue(5L, TimeUnit.SECONDS) {
+        moduleSet.distinct()
+    }
 
     override fun preLoad0(): List<Class<out Module>> {
         val stopTimer = StopTimer()
@@ -35,13 +41,10 @@ object ModuleManager : AsyncLoader<List<Class<out Module>>> {
 
     fun onBind(eventKey: Int) {
         if (eventKey == 0 || Keyboard.isKeyDown(Keyboard.KEY_F3)) return  // if key is the 'none' key (stuff like mod key in i3 might return 0)
-        for (module in getModules()) {
+        for (module in modules) {
             if (module.bind.value.isDown(eventKey)) module.toggle()
         }
     }
-
-    @JvmStatic
-    fun getModules() = moduleSet.toSet()
 
     fun getModuleOrNull(moduleName: String?) = moduleName?.let{ moduleSet[it] }
 }
