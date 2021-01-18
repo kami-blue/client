b9f7020e50280c755194f80174aae2b578a1fc82
scorbett123 <50634068+scorbett123@users.noreply.github.com>
2021-01-18 12:59:31 +0000

    suggested changes.

--- src/main/java/me/zeroeightsix/kami/module/modules/misc/AntiAFK.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/misc/AntiAFK.kt
@@ -39,15 +39,15 @@ internal object AntiAFK : Module(
     private val walk = setting("Walk", true)
     private val radius by setting("Radius", 64, 8..128, 8)
     private val inputTimeout by setting("InputTimeout(m)", 0, 0..15, 1)
-    private val allowBreak by setting("AllowBreak", false)
+    private val allowBreak by setting("AllowBreak", false, { walk.value })
 
     private var startPos: BlockPos? = null
     private var squareStep = 0
+    private var baritoneAllowBreak = false
     private var baritoneDisconnectOnArrival = false
     private val inputTimer = TickTimer(TimeUnit.MINUTES)
     private val actionTimer = TickTimer(TimeUnit.TICKS)
     private var nextActionDelay = 0
-    private var wasAllowBreakAllowed = false
 
     override fun getHudInfo(): String {
         return if (inputTimeout == 0) ""
@@ -56,7 +56,7 @@ internal object AntiAFK : Module(
 
     init {
         onEnable {
-            wasAllowBreakAllowed = BaritoneUtils.settings?.allowBreak?.value?:true
+            baritoneAllowBreak = BaritoneUtils.settings?.allowBreak?.value ?: true
             if (!allowBreak) BaritoneUtils.settings?.allowBreak?.value = false
             inputTimer.reset()
             baritoneDisconnectOnArrival()
@@ -64,7 +64,7 @@ internal object AntiAFK : Module(
 
         onDisable {
             startPos = null
-            BaritoneUtils.settings?.allowBreak?.value = wasAllowBreakAllowed
+            BaritoneUtils.settings?.allowBreak?.value = baritoneAllowBreak
             BaritoneUtils.settings?.disconnectOnArrival?.value = baritoneDisconnectOnArrival
             BaritoneUtils.cancelEverything()
         }
