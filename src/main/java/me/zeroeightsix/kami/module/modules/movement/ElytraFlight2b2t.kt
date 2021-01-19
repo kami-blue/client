eff4ca06f0c899da4afafe0d9769bf7192bdbcc6
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-01-18 21:42:39 -0500

    Send legit yaw, added constant acceleration, only response to position rubber band, don fly with creative fly

--- src/main/java/me/zeroeightsix/kami/module/modules/movement/ElytraFlight2b2t.kt
+++ src/main/java/me/zeroeightsix/kami/module/modules/movement/ElytraFlight2b2t.kt
@@ -14,7 +14,6 @@ import me.zeroeightsix.kami.util.TickTimer
 import me.zeroeightsix.kami.util.WorldUtils.getGroundPos
 import me.zeroeightsix.kami.util.math.Vec2f
 import me.zeroeightsix.kami.util.text.MessageSendHelper.sendChatMessage
-import me.zeroeightsix.kami.util.threads.runSafe
 import me.zeroeightsix.kami.util.threads.safeListener
 import net.minecraft.init.Items
 import net.minecraft.network.play.client.CPacketConfirmTeleport
@@ -26,7 +25,6 @@ import net.minecraft.util.math.Vec3d
 import org.kamiblue.commons.extension.toRadian
 import java.util.*
 import kotlin.collections.HashSet
-import kotlin.math.PI
 import kotlin.math.cos
 import kotlin.math.sin
 
@@ -35,8 +33,11 @@ internal object ElytraFlight2b2t : Module(
     description = "Allows high speed infinite Elytra flight with no durability usage on 2b2t",
     category = Category.MOVEMENT
 ) {
-    private val accelerateTime by setting("AccelerateTime", 7.0f, 0.5f..20.0f, 0.01f)
-    private val speed by setting("Speed", 3.28f, 1.0f..3.28f, 0.005f)
+    private val constantFactor by setting("ConstantFactor", ConstantFactor.ACCELERATION)
+    private val accelerateSpeed by setting("AccelerateSpeed", 0.08, 0.005..1.0, 0.005, { constantFactor == ConstantFactor.ACCELERATION })
+    private val accelerateTime by setting("AccelerateTime", 7.0, 0.5..30.0, 0.5, { constantFactor == ConstantFactor.TIME })
+
+    private val speed by setting("Speed", 2.7, 0.5..5.0, 0.05)
     private val descendSpeed by setting("DescendSpeed", 0.2, 0.01..1.0, 0.01)
     private val idleSpeed by setting("IdleSpeed", 3.8f, 0.1f..5.0f, 0.1f)
     private val idleRadius by setting("IdleRadius", 0.08f, 0.01f..0.25f, 0.01f)
@@ -44,6 +45,10 @@ internal object ElytraFlight2b2t : Module(
     private val packetDelay by setting("PacketDelay", 122, 50..200, 1)
     private val rubberbandTimeout by setting("RubberbandTimeout", 300, 0..1000, 1)
     private val showDebug by setting("ShowDebug", false)
+
+    private enum class ConstantFactor {
+        ACCELERATION, TIME
+    }
 
     private const val TAKEOFF_HEIGHT = 0.50
 
@@ -55,6 +60,7 @@ internal object ElytraFlight2b2t : Module(
     /* Startup variables */
     private var state = MovementState.NOT_STARTED
     private var accelerateStart = 0L
+    private var currentSpeed = 0.0
 
     /* Emergency teleport packet info */
     private val rubberbandTimer = TickTimer()
@@ -71,7 +77,7 @@ internal object ElytraFlight2b2t : Module(
 
     init {
         onDisable {
-            reset()
+            resetAll()
         }
 
         /**
@@ -85,41 +91,27 @@ internal object ElytraFlight2b2t : Module(
 
             if (state != MovementState.NOT_STARTED) {
                 /* If we are not wearing an elytra then reset */
-                if (player.inventory.armorInventory[2].item != Items.ELYTRA) {
-                    reset()
+                if (player.inventory.armorInventory[2].item != Items.ELYTRA || player.onGround) {
+                    resetAll()
                     return@safeListener
                 }
 
-                /* If we are too close to the ground then reset */
-                if (player.posY - getGroundPos().y < TAKEOFF_HEIGHT) {
-                    reset()
-                    return@safeListener
-                }
-
-                player.motionY = 0.0
-                player.capabilities.isFlying = true
+                player.setVelocity(0.0, 0.0, 0.0)
+                player.limbSwing = 0.0f
+                player.limbSwingAmount = 0.0f
+                player.prevLimbSwingAmount = 0.0f
+                it.cancel()
 
                 if (!rubberbandTimer.tick(rubberbandTimeout.toLong(), false)) {
-                    player.setVelocity(0.0, 0.0, 0.0)
-                    player.limbSwing = 0.0f
-                    player.limbSwingAmount = 0.0f
-                    player.prevLimbSwingAmount = 0.0f
-
-                    accelerateStart = System.currentTimeMillis()
+                    resetAcceleration()
                     rotation = Vec2f(teleportRotation.x, teleportRotation.y)
 
-                    it.cancel()
                     return@safeListener
                 }
 
                 if (player.movementInput.sneak) {
-                    player.setVelocity(0.0, -descendSpeed, 0.0)
+                    player.motionY = -descendSpeed
                     return@safeListener
-                }
-
-                /* Prevent the player from sprinting */
-                if (player.isSprinting) {
-                    player.isSprinting = false
                 }
             }
 
@@ -133,7 +125,7 @@ internal object ElytraFlight2b2t : Module(
 
     private fun SafeClientEvent.calcYaw(): Double {
         val yawDeg = calcMoveYawDeg()
-        rotation = Vec2f(yawDeg.toFloat(), player.pitchYaw.x)
+        rotation = Vec2f(player.rotationYaw, player.rotationPitch)
         return yawDeg.toRadian()
     }
 
@@ -148,49 +140,62 @@ internal object ElytraFlight2b2t : Module(
             && player.inventory.armorInventory[2].item == Items.ELYTRA
             && player.posY - getGroundPos().y >= TAKEOFF_HEIGHT) {
 
-            if (showDebug) sendChatMessage("$chatName Takeoff at height: " + player.posY)
-
             connection.sendPacket(CPacketEntityAction(player, CPacketEntityAction.Action.START_FALL_FLYING))
-            player.capabilities.isFlying = true
-            state = MovementState.IDLE
+
+            if (player.isElytraFlying) {
+                if (showDebug) sendChatMessage("$chatName Takeoff at height: " + player.posY)
+                state = MovementState.IDLE
+            }
         }
     }
 
     private fun SafeClientEvent.idle() {
         if (isInputting) {
             state = MovementState.MOVING
-            accelerateStart = System.currentTimeMillis()
+            resetAcceleration()
         } else {
             val length = idleSpeed * 1000.0f
             val deltaTime = System.currentTimeMillis() % length.toLong()
-            val yaw = deltaTime / length * 2.0 * PI
+            val yaw = deltaTime / length * 2.0 * Math.PI
 
             player.motionX = -sin(yaw) * idleRadius
             player.motionZ = cos(yaw) * idleRadius
-            player.motionY = 0.0
         }
     }
 
     private fun SafeClientEvent.moving(yawRad: Double) {
         if (isInputting) {
-            val length = accelerateTime * 1000.0f
-            val multiplier = ((System.currentTimeMillis() - accelerateStart) / length).coerceAtMost(1.0f)
-            val multipliedSpeed = speed * multiplier
+            val acceleratedSpeed = getAcceleratedSpeed()
 
-            player.motionX = -sin(yawRad) * multipliedSpeed
-            player.motionZ = cos(yawRad) * multipliedSpeed
-            player.motionY = 0.0
+            player.motionX = -sin(yawRad) * acceleratedSpeed
+            player.motionZ = cos(yawRad) * acceleratedSpeed
         } else {
-            accelerateStart = System.currentTimeMillis()
+            resetAcceleration()
             if (player.speed < minIdleVelocity) {
                 state = MovementState.IDLE
             }
         }
     }
 
+    private fun getAcceleratedSpeed(): Double {
+        return if (constantFactor == ConstantFactor.ACCELERATION) {
+            currentSpeed = if (currentSpeed > speed) {
+                (currentSpeed - accelerateSpeed / 20.0).coerceAtLeast(speed)
+            } else {
+                (currentSpeed + accelerateSpeed / 20.0).coerceAtMost(speed)
+            }
+            currentSpeed
+        } else {
+            val length = accelerateTime * 1000.0
+            val multiplier = ((System.currentTimeMillis() - accelerateStart) / length).coerceAtMost(1.0)
+            speed * multiplier
+        }
+    }
+
     init {
         safeListener<PacketEvent.Receive> {
-            if (state == MovementState.NOT_STARTED) return@safeListener
+            if (state == MovementState.NOT_STARTED || player.onGround) return@safeListener
+
             when (it.packet) {
                 /* Cancels the elytra opening animation */
                 is SPacketEntityMetadata -> {
@@ -198,13 +203,57 @@ internal object ElytraFlight2b2t : Module(
                 }
                 /* Set client side to wherever the server wants us to be */
                 is SPacketPlayerPosLook -> {
-                    rubberbandTimer.reset()
-                    teleportRotation = Vec2f(it.packet.yaw, it.packet.pitch)
-                    teleportPosition = Vec3d(it.packet.x, it.packet.y, it.packet.z)
+                    teleportPosition = getRubberbandPosition(it.packet)
+                    teleportRotation = getRubberbandRotation(it.packet)
+
+                    if (teleportPosition.distanceTo(player.positionVector) > 1.0) {
+                        if (showDebug) sendChatMessage("$chatName Position rubberband!")
+                        rubberbandTimer.reset()
+                        resetAcceleration()
+                    } else {
+                        if (showDebug) sendChatMessage("$chatName Rotation rubberband!")
+                    }
                 }
             }
         }
+    }
 
+    private fun SafeClientEvent.getRubberbandPosition(packetIn: SPacketPlayerPosLook) : Vec3d {
+        var posX = packetIn.x
+        var posY = packetIn.y
+        var posZ = packetIn.z
+
+        if (packetIn.flags.contains(SPacketPlayerPosLook.EnumFlags.X)) {
+            posX += player.posX
+        }
+
+        if (packetIn.flags.contains(SPacketPlayerPosLook.EnumFlags.Y)) {
+            posY += player.posY
+        }
+
+        if (packetIn.flags.contains(SPacketPlayerPosLook.EnumFlags.Z)) {
+            posZ += player.posZ
+        }
+
+        return Vec3d(posX, posY, posZ)
+    }
+
+    private fun SafeClientEvent.getRubberbandRotation(packetIn: SPacketPlayerPosLook) : Vec2f {
+        var yaw = packetIn.yaw
+        var pitch = packetIn.pitch
+
+        if (packetIn.flags.contains(SPacketPlayerPosLook.EnumFlags.X_ROT)) {
+            pitch += player.rotationPitch
+        }
+
+        if (packetIn.flags.contains(SPacketPlayerPosLook.EnumFlags.Y_ROT)) {
+            yaw += player.rotationYaw
+        }
+
+        return Vec2f(yaw, pitch)
+    }
+
+    init {
         /**
          * Cancel any packets that are not set directly by this module
          */
@@ -220,13 +269,7 @@ internal object ElytraFlight2b2t : Module(
          */
         safeListener<PacketEvent.PostSend> {
             if (state == MovementState.NOT_STARTED || it.packet !is CPacketConfirmTeleport) return@safeListener
-            if (showDebug) sendChatMessage("$chatName Responding to emergency teleport packet from the server.")
 
-//            player.setVelocity(0.0, 0.0, 0.0) Do we really need this? Worked for me without. Still no velo on rubberband
-            accelerateStart = System.currentTimeMillis()
-
-            /* This only sets the position and rotation client side since it is not salted with onGround */
-            player.setPositionAndRotation(teleportPosition.x, teleportPosition.y, teleportPosition.z, teleportRotation.x, teleportRotation.y)
             rotation = Vec2f(teleportRotation.x, teleportRotation.y)
 
             /* Force send the packet */
@@ -247,18 +290,20 @@ internal object ElytraFlight2b2t : Module(
         player.connection.sendPacket(packet)
     }
 
-    private fun reset() {
-        runSafe {
-            player.capabilities.isFlying = false
-        }
-
+    private fun resetAll() {
         packetSet.clear()
         rotation = Vec2f.ZERO
 
         state = MovementState.NOT_STARTED
-        accelerateStart = 0L
+        resetAcceleration()
 
         teleportPosition = Vec3d.ZERO
         teleportRotation = Vec2f.ZERO
     }
+
+    private fun resetAcceleration() {
+        accelerateStart = System.currentTimeMillis()
+        currentSpeed = 0.0
+    }
+
 }
