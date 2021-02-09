f13e2293e11c97d4b978de576dfae86d53a9b849
scorbett123 <50634068+scorbett123@users.noreply.github.com>
2021-02-09 10:14:06 +0000

    [enhancement] Add a modified setting command, that allows clicking to reset.
    Signed-off-by: scorbett123 <50634068+scorbett123@users.noreply.github.com>

---
+++ src/main/kotlin/org/kamiblue/client/command/commands/ModifiedCommand.kt
@@ -0,0 +1,32 @@
+package org.kamiblue.client.command.commands
+
+import net.minecraft.util.text.TextComponentString
+import net.minecraft.util.text.event.ClickEvent
+import net.minecraft.util.text.event.HoverEvent
+import org.kamiblue.client.command.ClientCommand
+import org.kamiblue.client.util.text.MessageSendHelper.sendChatMessage
+
+object ModifiedCommand: ClientCommand(
+    name = "modified",
+    description = "View modified settings in a module"
+) {
+
+    init{
+        module("module"){
+            execute("List changed settings"){
+
+                for (setting in it.value.settingList.filter { it.value != it.defaultValue }){
+                    val component = TextComponentString("${setting.name} has been changed to ${setting.value}")
+                    // horrible, however this is mojang code that we are working on.
+                    component.style.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, ";set ${it.value.name} ${setting.name.replace(" ", "")} ${setting.defaultValue}")
+                    component.style.hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponentString("Click to reset to default"))
+                    sendChatMessage(component)
+                }
+
+                if (!it.value.settingList.any { it.value != it.defaultValue }){
+                    sendChatMessage("No settings have been changed.")
+                }
+            }
+        }
+    }
+}
