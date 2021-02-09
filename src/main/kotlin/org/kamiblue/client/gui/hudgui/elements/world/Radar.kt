76f03a9093ca01801d261942fee36f66ad3e5540
zuim <git@zuim.de>
2021-02-09 06:05:59 +0100

    use SafeClientEvent for null safety rename squareInRadius remove backgroundColor Setting

--- src/main/kotlin/org/kamiblue/client/gui/hudgui/elements/world/Radar.kt
+++ src/main/kotlin/org/kamiblue/client/gui/hudgui/elements/world/Radar.kt
@@ -1,8 +1,10 @@
 package org.kamiblue.client.gui.hudgui.elements.world
 
 import net.minecraft.entity.EntityLivingBase
+import org.kamiblue.client.event.SafeClientEvent
 import org.kamiblue.client.gui.hudgui.HudElement
 import org.kamiblue.client.manager.managers.FriendManager
+import org.kamiblue.client.module.modules.client.GuiColors
 import org.kamiblue.client.module.modules.render.NewChunks
 import org.kamiblue.client.setting.GuiConfig.setting
 import org.kamiblue.client.util.EntityUtils
@@ -37,7 +39,6 @@ object Radar : HudElement(
     private val hostile = setting("Hostile Mobs", true)
     private val invisible = setting("Invisible Entities", true)
 
-    private val backgroundColor by setting("Background color", ColorHolder(28, 28, 28, 200), true)
     private val chunkGridColor by setting("Chunk grid color", ColorHolder(255, 0, 0, 100), true)
     private val distantChunkColor by setting("Distant chunk color", ColorHolder(100, 100, 100, 100), true, description = "Chunks that are not in render distance")
     private val newChunkColor by setting("New chunk color", ColorHolder(255, 0, 0, 100), true)
@@ -57,23 +58,23 @@ object Radar : HudElement(
         }
     }
 
-    private fun drawBorder(vertexHelper: VertexHelper) {
+    private fun SafeClientEvent.drawBorder(vertexHelper: VertexHelper) {
         glTranslated(radius.toDouble(), radius.toDouble(), 0.0)
-        drawCircleFilled(vertexHelper, radius = radius.toDouble(), color = backgroundColor)
+        drawCircleFilled(vertexHelper, radius = radius.toDouble(), color = GuiColors.backGround)
         drawCircleOutline(vertexHelper, radius = radius.toDouble(), lineWidth = 1.8f, color = primaryColor)
-        glRotatef(mc.player.rotationYaw + 180, 0f, 0f, -1f)
+        glRotatef(player.rotationYaw + 180, 0f, 0f, -1f)
     }
 
-    private fun drawNewChunks(vertexHelper: VertexHelper) {
-        val playerOffset = Vec2d((mc.player.posX - (mc.player.chunkCoordX shl 4)), (mc.player.posZ - (mc.player.chunkCoordZ shl 4)))
+    private fun SafeClientEvent.drawNewChunks(vertexHelper: VertexHelper) {
+        val playerOffset = Vec2d((player.posX - (player.chunkCoordX shl 4)), (player.posZ - (player.chunkCoordZ shl 4)))
         val chunkDist = (radius * radarScale).toInt() shr 4
         for (chunkX in -chunkDist..chunkDist) {
             for (chunkZ in -chunkDist..chunkDist) {
                 val pos0 = getChunkPos(chunkX, chunkZ, playerOffset)
                 val pos1 = getChunkPos(chunkX + 1, chunkZ + 1, playerOffset)
 
-                if (squareInRadius(pos0, pos1)) {
-                    val chunk = mc.world.getChunk(mc.player.chunkCoordX + chunkX, mc.player.chunkCoordZ + chunkZ)
+                if (isSquareInRadius(pos0, pos1)) {
+                    val chunk = world.getChunk(player.chunkCoordX + chunkX, player.chunkCoordZ + chunkZ)
                     if (!chunk.isLoaded)
                         drawRectFilled(vertexHelper, pos0, pos1, distantChunkColor)
                     drawRectOutline(vertexHelper, pos0, pos1, 0.3f, chunkGridColor)
@@ -82,22 +83,22 @@ object Radar : HudElement(
         }
 
         for (chunk in NewChunks.chunks) {
-            val pos0 = getChunkPos(chunk.x - mc.player.chunkCoordX, chunk.z - mc.player.chunkCoordZ, playerOffset)
-            val pos1 = getChunkPos(chunk.x - mc.player.chunkCoordX + 1, chunk.z - mc.player.chunkCoordZ + 1, playerOffset)
+            val pos0 = getChunkPos(chunk.x - player.chunkCoordX, chunk.z - player.chunkCoordZ, playerOffset)
+            val pos1 = getChunkPos(chunk.x - player.chunkCoordX + 1, chunk.z - player.chunkCoordZ + 1, playerOffset)
 
-            if (squareInRadius(pos0, pos1)) {
+            if (isSquareInRadius(pos0, pos1)) {
                 drawRectFilled(vertexHelper, pos0, pos1, newChunkColor)
             }
         }
     }
 
-    private fun drawEntities(vertexHelper: VertexHelper) {
+    private fun SafeClientEvent.drawEntities(vertexHelper: VertexHelper) {
         drawCircleFilled(vertexHelper, radius = 1.0, color = primaryColor) //player marker
-        val player = arrayOf(players.value, true, true) //enable friends and sleeping
-        val mob = arrayOf(true, passive.value, neutral.value, hostile.value) //enable mobs
 
-        for (entity in EntityUtils.getTargetList(player, mob, invisible.value, radius * radarScale, ignoreSelf = true)) {
-            val entityPosDelta = entity.position.subtract(mc.player.position)
+        val playerTargets = arrayOf(players.value, true, true) //enable friends and sleeping
+        val mobTargets = arrayOf(true, passive.value, neutral.value, hostile.value) //enable mobs
+        for (entity in EntityUtils.getTargetList(playerTargets, mobTargets, invisible.value, radius * radarScale, ignoreSelf = true)) {
+            val entityPosDelta = entity.position.subtract(player.position)
             if (abs(entityPosDelta.y) > 30) continue
             drawCircleFilled(vertexHelper, Vec2d(entityPosDelta.x.toDouble(), entityPosDelta.z.toDouble()).div(radarScale.toDouble()), 2.5 / radarScale, color = getColor(entity))
         }
@@ -124,7 +125,7 @@ object Radar : HudElement(
     }
 
     // p2.x > p1.x and p2.y > p1.y is assumed
-    private fun squareInRadius(p1: Vec2d, p2: Vec2d): Boolean {
+    private fun isSquareInRadius(p1: Vec2d, p2: Vec2d): Boolean {
         return if ((p1.x + p2.x) / 2 > 0) {
             if ((p1.y + p2.y) / 2 > 0) {
                 p2.length()
