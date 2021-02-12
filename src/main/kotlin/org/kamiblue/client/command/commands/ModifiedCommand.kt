124324079b8b18011d31f0a9569139a2877e2109
scorbett123 <50634068+scorbett123@users.noreply.github.com>
2021-02-12 09:49:16 +0000

    More suggested changes
    Signed-off-by: scorbett123 <50634068+scorbett123@users.noreply.github.com>

--- src/main/kotlin/org/kamiblue/client/command/commands/ModifiedCommand.kt
+++ src/main/kotlin/org/kamiblue/client/command/commands/ModifiedCommand.kt
@@ -4,6 +4,7 @@ import net.minecraft.util.text.TextComponentString
 import net.minecraft.util.text.event.ClickEvent
 import net.minecraft.util.text.event.HoverEvent
 import org.kamiblue.client.command.ClientCommand
+import org.kamiblue.client.module.modules.client.CommandConfig
 import org.kamiblue.client.util.text.MessageSendHelper.sendChatMessage
 
 object ModifiedCommand: ClientCommand(
@@ -12,17 +13,18 @@ object ModifiedCommand: ClientCommand(
 ) {
     init {
         module("module") {
-            execute("List changed settings") {
-
+            execute("List modified settings") {
                 var anyChanged = false
 
                 for (setting in it.value.settingList) {
                     if (!setting.isModified) continue
                     anyChanged = true
+
                     val component = TextComponentString("${setting.name} has been changed to ${setting.value}")
                     // horrible, however this is mojang code that we are working on.
-                    component.style.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, ";set ${it.value.name} ${setting.name.replace(" ", "")} ${setting.defaultValue}")
-                    component.style.hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponentString("Click to reset to default"))
+                    component.style.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "${CommandConfig.prefix.value}set ${it.value.name} ${setting.name.replace(" ", "")} ${setting.defaultValue}")
+                    component.style.hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponentString("Click to reset to default settings"))
+
                     sendChatMessage(component)
                 }
 
