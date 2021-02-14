19348cb52b77441ec1a52333844569376c1c457c
scorbett123 <50634068+scorbett123@users.noreply.github.com>
2021-02-14 11:20:38 +0000

    suggested changes
    Signed-off-by: scorbett123 <50634068+scorbett123@users.noreply.github.com>

--- src/main/kotlin/org/kamiblue/client/command/commands/ModifiedCommand.kt
+++ src/main/kotlin/org/kamiblue/client/command/commands/ModifiedCommand.kt
@@ -4,6 +4,7 @@ import net.minecraft.util.text.TextComponentString
 import net.minecraft.util.text.event.ClickEvent
 import net.minecraft.util.text.event.HoverEvent
 import org.kamiblue.client.command.ClientCommand
+import org.kamiblue.client.command.CommandManager
 import org.kamiblue.client.module.modules.client.CommandConfig
 import org.kamiblue.client.util.text.MessageSendHelper.sendChatMessage
 
@@ -12,24 +13,24 @@ object ModifiedCommand: ClientCommand(
     description = "View modified settings in a module"
 ) {
     init {
-        module("module") {
+        module("module") { module ->
             execute("List modified settings") {
                 var anyChanged = false
 
-                for (setting in it.value.settingList) {
+                for (setting in module.value.settingList) {
                     if (!setting.isModified) continue
                     anyChanged = true
 
                     val component = TextComponentString("${setting.name} has been changed to ${setting.value}")
                     // horrible, however this is mojang code that we are working on.
-                    component.style.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "${CommandConfig.prefix.value}set ${it.value.name} ${setting.name.replace(" ", "")} ${setting.defaultValue}")
+                    component.style.clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "${CommandManager.prefix}set ${module.value.name} ${setting.name.replace(" ", "")} ${setting.defaultValue}")
                     component.style.hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponentString("Click to reset to default settings"))
 
                     sendChatMessage(component)
                 }
 
                 if (!anyChanged) {
-                    sendChatMessage("${it.value.name}'s settings are not modified from default")
+                    sendChatMessage("${module.value.name}'s settings are not modified from default")
                 }
             }
         }
