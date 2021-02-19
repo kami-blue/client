61e76841536b6fce866b3daeece32416f9537a6d
Xiaro <62033805+Xiaro@users.noreply.github.com>
2021-02-19 16:16:03 -0500

    Added runInGui, timeout fields to click task

--- src/main/kotlin/org/kamiblue/client/util/inventory/Task.kt
+++ src/main/kotlin/org/kamiblue/client/util/inventory/Task.kt
@@ -15,35 +15,41 @@ fun SafeClientEvent.clickTaskNow(block: ClickTask.Builder.() -> Unit) =
     }
 
 fun AbstractModule.clickTask(block: ClickTask.Builder.() -> Unit) =
-    ClickTask.Builder().apply { priority(modulePriority) }.apply(block).build()
+    ClickTask.Builder().apply { priority(modulePriority) }.apply(block).build().also {
+        InventoryClickManager.addTask(it)
+    }
 
-private fun clickTask(block: ClickTask.Builder.() -> Unit) =
-    ClickTask.Builder().apply(block).build()
+val ClickTask?.executedOrTrue get() = this == null || this.executed
+
+val ClickTask?.confirmedOrTrue get() = this == null || this.confirmed
 
 class ClickTask private constructor(
     private val id: Int,
     private val priority: Int,
     val delay: Long,
     val postDelay: Long,
+    val timeout: Long,
+    val runInGui: Boolean,
     private val clicks: Array<ClickInfo>
 ) : Comparable<ClickTask> {
     val executed by ComputeFlag {
-        cancelled || index >= clicks.size
+        cancelled || System.currentTimeMillis() - finishTime > postDelay
     }
     val confirmed by ComputeFlag {
-        cancelled || futures.all { it?.confirmed ?: false }
+        cancelled || executed && futures.all { it?.timeout(timeout + postDelay) ?: false }
     }
 
     private val futures = arrayOfNulls<ClickFuture?>(clicks.size)
+    private var finishTime = -1L
     private var index = 0
     private var cancelled = false
 
-    fun timeout(timeout: Long): Boolean {
-        return confirmed || futures.all { it?.timeout(timeout) ?: false }
-    }
-
     fun runTask(event: SafeClientEvent): ClickFuture? {
-        if (executed) return null
+        if (cancelled || index >= clicks.size) {
+            if (finishTime == -1L) finishTime = System.currentTimeMillis()
+            return null
+        }
+
         val currentIndex = index++
         return clicks[currentIndex].runClick(event).also { futures[currentIndex] = it }
     }
@@ -63,22 +69,33 @@ class ClickTask private constructor(
 
     override fun hashCode() = id
 
+
     class Builder {
         private val clicks = ArrayList<ClickInfo>()
         private var priority = 0
         private var delay = 50L
         private var postDelay = 100L
+        private var timeout = 3000L
+        private var runInGui = false
 
         fun priority(value: Int) {
             priority = value
         }
 
-        fun delay(value: Long, timeUnit: TimeUnit) {
+        fun delay(value: Long, timeUnit: TimeUnit = TimeUnit.MILLISECONDS) {
             delay = value * timeUnit.multiplier
         }
 
-        fun postDelay(value: Long, timeUnit: TimeUnit) {
-            delay = value * timeUnit.multiplier
+        fun postDelay(value: Long, timeUnit: TimeUnit = TimeUnit.MILLISECONDS) {
+            postDelay = value * timeUnit.multiplier
+        }
+
+        fun timeout(value: Long, timeUnit: TimeUnit = TimeUnit.MILLISECONDS) {
+            timeout = value * timeUnit.multiplier
+        }
+
+        fun runInGui() {
+            runInGui = true
         }
 
         operator fun ClickInfo.unaryPlus() {
@@ -86,7 +103,7 @@ class ClickTask private constructor(
         }
 
         fun build(): ClickTask {
-            return ClickTask(currentID++, priority, delay, postDelay, clicks.toTypedArray())
+            return ClickTask(currentID++, priority, delay, postDelay, timeout, runInGui, clicks.toTypedArray())
         }
     }
 
@@ -126,5 +143,4 @@ class ClickFuture(
     fun confirm() {
         confirmed = true
     }
-
 }
