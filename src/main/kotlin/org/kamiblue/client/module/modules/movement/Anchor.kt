84ac15d15df8e6265786438c61b52f13a3a9955f
Toshimichi0915 <main@toshimichi.net>
2021-02-28 20:47:47 +0900

    Revert "Revert "Write better code""

--- src/main/kotlin/org/kamiblue/client/module/modules/movement/Anchor.kt
+++ src/main/kotlin/org/kamiblue/client/module/modules/movement/Anchor.kt
@@ -35,8 +35,8 @@ internal object Anchor : Module(
     /**
      * Checks whether the specified block position is a hole or not
      */
-    private fun isHole(event: SafeClientEvent, pos: BlockPos): Boolean {
-        val type = event.checkHole(pos)
+    private fun SafeClientEvent.isHole(pos: BlockPos): Boolean {
+        val type = checkHole(pos)
         return ((mode == AnchorMode.BOTH && type != SurroundUtils.HoleType.NONE) ||
             (mode == AnchorMode.BEDROCK && type == SurroundUtils.HoleType.BEDROCK))
     }
@@ -53,11 +53,11 @@ internal object Anchor : Module(
     /**
      * Checks whether the player should stop movement or not
      */
-    private fun shouldStop(event: SafeClientEvent): Boolean {
+    private fun SafeClientEvent.shouldStop(): Boolean {
         if (pitchTrigger && mc.player.rotationPitch < MathHelper.wrapDegrees(pitch)) return false
         for (dy in 1..vRange) {
             val pos = mc.player.positionVector.toBlockPos().add(0.0, -dy.toDouble(), 0.0)
-            if (isHole(event, pos) && isReachable(pos)) {
+            if (isHole(pos)) {
                 return true
             }
         }
@@ -70,7 +70,7 @@ internal object Anchor : Module(
         }
 
         safeListener<PlayerTravelEvent> {
-            if (isHole(this, player.positionVector.toBlockPos()) &&
+            if (isHole(player.positionVector.toBlockPos()) &&
                 !world.isAirBlock(player.positionVector.add(0.0, -0.1, 0.0).toBlockPos())) {
                 prevInHole = true
                 if (turnOffAfter)
@@ -78,7 +78,7 @@ internal object Anchor : Module(
                 return@safeListener
             }
             for (dy in -vRange..0) {
-                if (!shouldStop(this)) {
+                if (!shouldStop()) {
                     prevInHole = false
                     return@safeListener
                 }
