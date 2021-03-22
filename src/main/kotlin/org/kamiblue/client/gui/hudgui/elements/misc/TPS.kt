18adf14004368ff9b2af182e69ad8e233f9ab689
Jacob Herd <53945697+ToxicAven@users.noreply.github.com>
2021-03-22 13:00:05 -0400

    Move MSPT into TPS as a setting

--- src/main/kotlin/org/kamiblue/client/gui/hudgui/elements/misc/TPS.kt
+++ src/main/kotlin/org/kamiblue/client/gui/hudgui/elements/misc/TPS.kt
@@ -2,6 +2,7 @@ package org.kamiblue.client.gui.hudgui.elements.misc
 
 import org.kamiblue.client.event.SafeClientEvent
 import org.kamiblue.client.gui.hudgui.LabelHud
+import org.kamiblue.client.gui.hudgui.elements.misc.MemoryUsage.setting
 import org.kamiblue.client.util.CircularArray
 import org.kamiblue.client.util.TpsCalculator
 
@@ -10,15 +11,20 @@ internal object TPS : LabelHud(
     category = Category.MISC,
     description = "Server TPS"
 ) {
+    private val mspt = setting("Show MSPT", false)
 
     // buffered TPS readings to add some fluidity to the TPS HUD element
     private val tpsBuffer = CircularArray.create(20, 20f)
 
     override fun SafeClientEvent.updateText() {
         tpsBuffer.add(TpsCalculator.tickRate)
-
-        displayText.add("%.2f".format(tpsBuffer.average()), primaryColor)
-        displayText.add("tps", secondaryColor)
+        if (mspt.value) {
+            displayText.add("%.2f".format(1000 / tpsBuffer.average()), primaryColor)
+            displayText.add("MS/Tick", secondaryColor)
+        } else {
+            displayText.add("%.2f".format(tpsBuffer.average()), primaryColor)
+            displayText.add("tps", secondaryColor)
+        }
     }
 
 }
