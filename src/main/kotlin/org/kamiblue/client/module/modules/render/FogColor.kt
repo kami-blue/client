568aeb69bf76f46344aebdb060850d759767c6ac
Jacob Herd <53945697+ToxicAven@users.noreply.github.com>
2021-03-24 15:10:12 -0400

    Disable Alpha

--- src/main/kotlin/org/kamiblue/client/module/modules/render/FogColor.kt
+++ src/main/kotlin/org/kamiblue/client/module/modules/render/FogColor.kt
@@ -11,7 +11,7 @@ internal object FogColor : Module(
     description = "Recolors render fog",
     category = Category.RENDER
 ) {
-    private val Color = setting("Color", ColorHolder(111, 166, 222, 255))
+    private val Color = setting("Color", ColorHolder(111, 166, 222), false)
 
     init {
         listener<EntityViewRenderEvent.FogColors> {
