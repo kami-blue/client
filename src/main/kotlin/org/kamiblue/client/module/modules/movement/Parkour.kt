c4877cfa5af6fa8458678da4bffb35a6449de80e
liv <sokolov.dominika@gmail.com>
2021-03-25 13:56:59 -0400

    Apply suggestions from code review

--- src/main/kotlin/org/kamiblue/client/module/modules/movement/Parkour.kt
+++ src/main/kotlin/org/kamiblue/client/module/modules/movement/Parkour.kt
@@ -5,17 +5,21 @@ import org.kamiblue.client.module.Category
 import org.kamiblue.client.module.Module
 import org.kamiblue.client.util.threads.safeListener
 
-
 internal object Parkour : Module(
     name = "Parkour",
-    description = "Jump at the edge of blocks",
+    description = "Automatically jump at the edge of blocks",
     category = Category.MOVEMENT
 ) {
     init {
         safeListener<TickEvent.ClientTickEvent> {
-            if(player.onGround &&
-            !player.isSneaking &&
-            world.getCollisionBoxes(player, player.entityBoundingBox.offset(0.0, -0.5, 0.0).expand(-0.001, 0.0, -0.001)).isEmpty())
-            player.jump() }
+            if (
+                player.onGround && !player.isSneaking &&
+                world.getCollisionBoxes(player, player.entityBoundingBox
+                    .offset(0.0, -0.5, 0.0)
+                    .expand(-0.001, 0.0, -0.001)).isEmpty()
+            ) {
+                player.jump()
+            }
         }
     }
+}
