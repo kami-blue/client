fe5170c73ee549e57099c6d2fdd5a581e54c6134
scorbett123 <50634068+scorbett123@users.noreply.github.com>
2021-04-02 11:19:21 +0100

    [enh] improve liquid interact
    Signed-off-by: scorbett123 <50634068+scorbett123@users.noreply.github.com>

--- src/main/java/org/kamiblue/client/mixin/client/world/MixinBlockLiquid.java
+++ src/main/java/org/kamiblue/client/mixin/client/world/MixinBlockLiquid.java
@@ -3,11 +3,13 @@ package org.kamiblue.client.mixin.client.world;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.kamiblue.client.module.modules.movement.Velocity;
 import org.kamiblue.client.module.modules.player.BlockInteraction;
+import org.kamiblue.client.util.Wrapper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,7 +26,7 @@ public class MixinBlockLiquid {
 
     @Inject(method = "canCollideCheck", at = @At("HEAD"), cancellable = true)
     public void canCollideCheck(IBlockState blockState, boolean hitIfLiquid, CallbackInfoReturnable<Boolean> cir) {
-        if (BlockInteraction.isLiquidInteractEnabled()) {
+        if (BlockInteraction.isLiquidInteractEnabled() && Wrapper.getMinecraft().player.getHeldItemMainhand().getItem() instanceof ItemBlock) {
             cir.setReturnValue(true);
         }
     }
