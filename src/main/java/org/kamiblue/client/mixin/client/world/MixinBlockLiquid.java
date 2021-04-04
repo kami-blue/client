33beb1b81c5bac90843797db3346a60582baa828
lv <~@l1v.in>
2021-04-04 19:56:37 -0400

    enh: Make customizable
    Signed-off-by: lv <~@l1v.in>

--- src/main/java/org/kamiblue/client/mixin/client/world/MixinBlockLiquid.java
+++ src/main/java/org/kamiblue/client/mixin/client/world/MixinBlockLiquid.java
@@ -3,13 +3,11 @@ package org.kamiblue.client.mixin.client.world;
 import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.kamiblue.client.module.modules.movement.Velocity;
 import org.kamiblue.client.module.modules.player.BlockInteraction;
-import org.kamiblue.client.util.Wrapper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -26,7 +24,7 @@ public class MixinBlockLiquid {
 
     @Inject(method = "canCollideCheck", at = @At("HEAD"), cancellable = true)
     public void canCollideCheck(IBlockState blockState, boolean hitIfLiquid, CallbackInfoReturnable<Boolean> cir) {
-        if (BlockInteraction.isLiquidInteractEnabled() && Wrapper.getMinecraft().player.getHeldItemMainhand().getItem() instanceof ItemBlock) {
+        if (BlockInteraction.isLiquidInteractEnabled()) {
             cir.setReturnValue(true);
         }
     }
