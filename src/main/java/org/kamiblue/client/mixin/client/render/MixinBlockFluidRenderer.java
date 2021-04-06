0eb79e60b28d0bffec6fa8ad172ac545c58300e4
lv <~@l1v.in>
2021-04-06 11:28:44 -0400

    cleanup code
    Signed-off-by: lv <~@l1v.in>

--- src/main/java/org/kamiblue/client/mixin/client/render/MixinBlockFluidRenderer.java
+++ src/main/java/org/kamiblue/client/mixin/client/render/MixinBlockFluidRenderer.java
@@ -5,19 +5,20 @@ import net.minecraft.client.renderer.BlockFluidRenderer;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import org.kamiblue.client.module.modules.render.Xray;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.kamiblue.client.module.modules.render.Xray;
 
 @Mixin(BlockFluidRenderer.class)
 public class MixinBlockFluidRenderer {
 
     @Inject(method = "renderFluid", at = @At("HEAD"), cancellable = true)
-    public void renderFluid(IBlockAccess blockAccess, IBlockState blockStateIn, BlockPos blockPosIn, BufferBuilder bufferBuilderIn, CallbackInfoReturnable<Boolean> cir) {
-        if (Xray.INSTANCE.isEnabled() && Xray.INSTANCE.shouldReplace(blockStateIn)) {
-            cir.cancel();
+    public void renderFluid(IBlockAccess blockAccess, IBlockState blockStateIn, BlockPos blockPosIn, BufferBuilder bufferBuilderIn, CallbackInfoReturnable<Boolean> ci) {
+        if (Xray.shouldReplace(blockStateIn)) {
+            ci.cancel();
         }
     }
+
 }
