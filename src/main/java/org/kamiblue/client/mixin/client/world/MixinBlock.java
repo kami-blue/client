d0c8da4af5e07a39f36b8ec07f1f8ac5ae90a9d1
lv <~@l1v.in>
2021-04-19 14:30:34 -0400

    format
    Signed-off-by: lv <~@l1v.in>

--- src/main/java/org/kamiblue/client/mixin/client/world/MixinBlock.java
+++ src/main/java/org/kamiblue/client/mixin/client/world/MixinBlock.java
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Block.class)
 public class MixinBlock {
-
     @Inject(method = "getLightValue(Lnet/minecraft/block/state/IBlockState;)I", at = @At("HEAD"), cancellable = true)
     public void getLightValue(IBlockState state, CallbackInfoReturnable<Integer> cir) {
         if (Xray.INSTANCE.isEnabled()) {
